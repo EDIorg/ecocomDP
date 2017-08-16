ef29f32d97aa2ce40bf1d89f7275750d59c2dc9e
mobb <mob@msi.ucsb.edu>
2017-08-16 14:27:50 -0700

    1. addressed issues based on comments from initial attempts to convert files (issues #1, 3, 4, 5, 6, 7). 2. added sql script to drop tables (prep for reload)

--- postgreSQL/create_7tables_ecocomDP_postgres.sql
+++ postgreSQL/create_7tables_ecocomDP_postgres.sql
@@ -7,13 +7,11 @@
 -- Dumped by pg_dump version 9.2.2
 -- Started on 2016-12-16 15:28:07 PST
 /*
-created file from pgdump:
-
-mcr-office02:schema_mini_metabase mob$ sed 's/metabase2/mini_metabase/g'  /Volumes/homes/backup/pg_dumps/rdb2_sbc_sandbox_attributeDictionary_schema_only2016dec16.sql > create_7_tables_schema.sql
+created original from pgdump, edited. see personal notes.
 */
 
 /*
-table population order - suggested (ie, parents first)
+table population order - suggested (ie, parents first).
 1. sampling_location
 2. taxon
 3. event
@@ -35,41 +33,27 @@ SET search_path = "ecocom_dp", pg_catalog;
 SET default_tablespace = '';
 
 SET default_with_oids = false;
-
-
 */
+
+/*
 -- Create 7 tables:
--- Table: "ecocom_dp".observation
+*/
 
--- DROP TABLE "ecocom_dp".observation;
 
--- DROP TABLE "ecocom_dp".observation;
-
-CREATE TABLE "ecocom_dp".observation (
-	observation_id integer NOT NULL,
-	event_id integer,
-	dataset_summary_id integer NOT NULL,
-	sampling_location_id integer NOT NULL,
-	observation_datetime timestamp without time zone,
-	taxon_id integer NOT NULL,
-	variable_name character varying(200) NOT NULL,
-	value float NOT NULL,
-	unit character varying(200) NOT NULL
-);
 -- DROP TABLE "ecocom_dp".sampling_location;
 CREATE TABLE "ecocom_dp".sampling_location (
-	sampling_location_id integer NOT NULL,
+	sampling_location_id character varying(100) NOT NULL,
 	sampling_location_name character varying(500),
 	latitude float,
 	longitude float,
-	elevation float,
-	parent_sampling_location_id integer 
+	elevation float, -- relative to sealevel
+	parent_sampling_location_id character varying(100) 
 );
 
 -- DROP TABLE "ecocom_dp".taxon;
 CREATE TABLE "ecocom_dp".taxon (
-	taxon_id integer NOT NULL,
-	taxon_level character varying(200),
+	taxon_id character varying(100) NOT NULL,
+	taxon_rank character varying(200),
 	taxon_name character varying(200) NOT NULL,
 	authority_system character varying(200),
 	authority_taxon_id character varying(200)
@@ -77,41 +61,54 @@ CREATE TABLE "ecocom_dp".taxon (
 
 -- DROP TABLE "ecocom_dp".event;
 CREATE TABLE "ecocom_dp".event (
-	event_id integer NOT NULL,
-	variable_name  character varying(200),
-	value  character varying(200)
-
+  event_record_id character varying(100) NOT NULL,
+	event_id character varying(100) NOT NULL,
+	variable_name character varying(200),
+	value character varying(200),
+	unit character varying(200)
 );
 
--- DROP TABLE "ecocom_dp".taxon_ancillary;
-CREATE TABLE "ecocom_dp".taxon_ancillary (
-    taxon_ancillary_id integer NOT NULL,
-	taxon_id integer NOT NULL,
-	datetime timestamp without time zone NOT NULL,
-    variable_name  character varying(200) NOT NULL,
-    value  character varying(200) NOT NULL,
-	author  character varying(200)
+-- DROP TABLE "ecocom_dp".observation;
+CREATE TABLE "ecocom_dp".observation (
+	observation_id character varying(100) NOT NULL,
+	event_record_id character varying(100) NOT NULL,
+	package_id character varying(100) NOT NULL,
+	sampling_location_id character varying(100) NOT NULL,
+	observation_datetime timestamp without time zone,
+	taxon_id character varying(100) NOT NULL,
+	variable_name character varying(200) NOT NULL,
+	value character varying(200) NOT NULL,
+	unit character varying(200) NOT NULL
 );
 
 -- DROP TABLE "ecocom_dp".sampling_location_ancillary;
 CREATE TABLE "ecocom_dp".sampling_location_ancillary (
-    sampling_location_ancillary_id integer NOT NULL,
-    sampling_location_id integer NOT NULL,
-    datetime timestamp without time zone NOT NULL,
+    sampling_location_ancillary_id character varying(100) NOT NULL,
+    sampling_location_id character varying(100) NOT NULL,
+    datetime timestamp without time zone NOT NULL, -- e.g. experimental treatment
     variable_name  character varying(200) NOT NULL,
     value  character varying(200) NOT NULL,
     unit  character varying(200)
 );
 
+-- DROP TABLE "ecocom_dp".taxon_ancillary;
+CREATE TABLE "ecocom_dp".taxon_ancillary (
+    taxon_ancillary_id character varying(100) NOT NULL,
+	  taxon_id character varying(100) NOT NULL,
+	  datetime timestamp without time zone NOT NULL,  
+    variable_name  character varying(200) NOT NULL,
+    value character varying(200) NOT NULL,
+	  author character varying(200)
+);
 
 -- DROP TABLE "ecocom_dp".dataset_summary;
 CREATE TABLE "ecocom_dp".dataset_summary (
-	dataset_summary_id integer NOT NULL,
-	original_dataset_id character varying(200),
-	length_of_survey_years integer NOT NULL,
-	number_of_years_sampled integer NOT NULL,
-	std_dev_interval_betw_years float NOT NULL,
-	max_num_taxa integer NOT NULL, 
+	package_id character varying(100) NOT NULL,
+	original_package_id character varying(200),
+	length_of_survey_years integer,
+	number_of_years_sampled integer,
+	std_dev_interval_betw_years float,
+	max_num_taxa integer, 
 	geo_extent_bounding_box_m2 float
 
 );
@@ -152,10 +149,10 @@ ALTER TABLE ONLY "ecocom_dp".taxon_ancillary
     ADD CONSTRAINT taxon_ancillary_pk PRIMARY KEY (taxon_ancillary_id);
 
 ALTER TABLE ONLY "ecocom_dp".dataset_summary
-    ADD CONSTRAINT dataset_summary_pk PRIMARY KEY (dataset_summary_id);
+    ADD CONSTRAINT dataset_summary_pk PRIMARY KEY (package_id);
 
 ALTER TABLE ONLY "ecocom_dp".event
-    ADD CONSTRAINT event_pk PRIMARY KEY (event_id);
+    ADD CONSTRAINT event_pk PRIMARY KEY (event_record_id);
 
 
 
@@ -169,10 +166,10 @@ ALTER TABLE ONLY "ecocom_dp".observation
     ADD CONSTRAINT observation_taxon_fk FOREIGN KEY (taxon_id) REFERENCES "ecocom_dp".taxon (taxon_id) MATCH SIMPLE     
     ON UPDATE CASCADE;
 ALTER TABLE ONLY "ecocom_dp".observation
-    ADD CONSTRAINT observation_event_fk FOREIGN KEY (event_id) REFERENCES "ecocom_dp".event (event_id) MATCH SIMPLE
+    ADD CONSTRAINT observation_event_record_fk FOREIGN KEY (event_record_id) REFERENCES "ecocom_dp".event (event_record_id) MATCH SIMPLE
     ON UPDATE CASCADE;
 ALTER TABLE ONLY "ecocom_dp".observation
-    ADD CONSTRAINT observation_dataset_summary_fk FOREIGN KEY (dataset_summary_id) REFERENCES "ecocom_dp".dataset_summary (dataset_summary_id) MATCH SIMPLE
+    ADD CONSTRAINT observation_package_fk FOREIGN KEY (package_id) REFERENCES "ecocom_dp".dataset_summary (package_id) MATCH SIMPLE
     ON UPDATE CASCADE;
 
 
