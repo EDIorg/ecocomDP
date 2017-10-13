bdcc5ecef41668051c298a67634b87e45127fc86
Colin Smith <colin.smith@wisc.edu>
2017-10-13 16:55:57 -0500

    Continue drafting function to clean taxon table.

--- R/clean_taxon.R
+++ R/clean_taxon.R
@@ -5,61 +5,114 @@
 #'
 #' @usage clean_taxon(path, parent.package.id, table.name, taxon.col)
 #' 
-#'     Run this function to clean up the taxonomy data of an input dataset
-#'     prior to running this information through the Integrated Taxonomic 
-#'     Information System (ITIS) to obtain rank values and a Taxonomic Serial
-#'     Number.
+#'     Run this function after making ecocomDP tables, which includes a draft
+#'     version of the taxon table.
 #'
 #' @param path 
 #'     A path to the dataset working directory where the clean taxon table will
 #'     be exported.
 #'     
-#' @param parent.package.id
-#'     Identifier of parent data package (e.g. knb-lter-hfr.118.28).
+#' @param delimiter
+#'     Delimiter of taxon table in the working directory. Can be comma (",") or
+#'     tab ("\t").
 #'     
-#' @param table.name
-#'     Full name of table from which the taxonomy data is stored.
-#'     
-#' @param taxon.col
-#'     Column names containing the taxonomy data.
+#' @param cleaning.step
+#'     Step in the data cleaning process. Can be L0, L1, L2, L3, L4.
 #'
 #' @return 
-#'     A tab delimited UTF-8 file in the ecocomDP working directory titled 
-#'     \emph{taxon_clean.txt} containing clean taxon names that can then be
-#'     used to query ITIS.
+#'     A tab delimited UTF-8 file in the ecocomDP working directory appended
+#'     with a title corresponding with the cleaning step level.
 #'     
 #' @details 
-#'     This function overwrites
-#'     \emph{taxon_clean.txt} files you have created in the ecocomDP 
-#'     working directory.
+#'     This function fails out if tables created with this workflow already
+#'     exist in the working director. Remove these files or rename to allow 
+#'     the function to continue.
 #'
 #' @export
 #'
 
 
-clean_taxon <- function(path, parent.package.id, table.name, col.name) {
+clean_taxon <- function(path, delimiter, cleaning.step) {
   
   # Check arguments
   
   if (missing(path)){
     stop("Specify path to dataset working directory.")
   }
-  if (missing(parent.package.id)){
-    stop("Specify path to dataset working directory.")
+  if (missing(delimiter)){
+    stop("Specify a delimiter to the ecocomDP taxon table in the working directory.")
+  }
+  if (missing(cleaning.step)){
+    stop("Specify a cleaning step.")
   }
   
   # Parameters ----------------------------------------------------------------
   
+  dir_files <- list.files(path)
+  valid_table_names <- c("taxon\\b",
+                         "taxon_L1\\b",
+                         "taxon_L2\\b",
+                         "taxon_L3\\b",
+                         "taxon_L4\\b")
+  input_table_names <- dir_files[attr(regexpr(paste(valid_table_names, 
+                                                    collapse = "|"), 
+                                              dir_files),
+                                      "match.length")
+                                 != -1]
   
-  # Begin function ------------------------------------------------------------
+  # Step 1 --------------------------------------------------------------------
+  # Get the taxon name from the raw dataset (in this case the taxon_name field
+  # of the ecocomDP taxon table) and append a column for an information manager
+  # to pass judgement on whether the listed taxon name has any taxonomic value.
+  # For example an entity listed as "stone" or "unsorted biomass" will not be 
+  # listed in a taxonomic authority and thus has no taxonomic value.
 
-  # Issue warning
-
-  answer <- readline(
-    "Are you sure you want to build new variable tables? This will overwrite your previous work! (y or n):  ")
-  
-  if (answer == "y"){
-    
+  if (cleaning.step == "1"){
+    if (identical(input_table_names[attr(regexpr("taxon_L1\\b", input_table_names), "match.length") != -1], character(0))){
+      stop("A taxon_L1 table already exists in this directory. Remove this file
+           or rename and try again.")
+    } else {
+      input_file_name <- input_table_names[attr(regexpr("taxon\\b", input_table_names), "match.length") != -1]
+      clean_taxon_step1 <- function(path, input_file_name, delimiter){
+        data_in <- read.table(paste(path,
+                                    "/",
+                                    input_file_name,
+                                    sep = ""),
+                              header = T,
+                              sep = delimiter,
+                              as.is = T,
+                              na.strings = "NA")
+        data_out <- data.frame(taxon_name = data_in[["taxon_name"]],
+                               meaningful = character(length(data_in[["taxon_name"]])))
+        if (delimiter == ","){
+          write.table(data_out,
+                      file = paste(path,
+                                   "/",
+                                   "taxon_L0.csv",
+                                   sep = ""),
+                      col.names = T,
+                      row.names = F,
+                      sep = ",",
+                      eol = "\r\n",
+                      quote = F)
+        } else if (delimiter == "\t"){
+          write.table(data_out,
+                      file = paste(path,
+                                   "/",
+                                   "taxon_L1.txt",
+                                   sep = ""),
+                      col.names = T,
+                      row.names = F,
+                      sep = "\t",
+                      eol = "\r\n",
+                      quote = F)
+        }
+        # Send table to taxize or IM for evaluation of meaningful entities?
+        
+        
+        
+      }
+    }
   }
 }
 
