6242791dba96084f4cbe6ebf18e9aab7ffe58697
Colin Smith <colin.smith@wisc.edu>
2017-10-17 11:17:33 -0500

    Differentiate between data and code paths, align other param names with base R, update documentation.

--- R/define_variables.R
+++ R/define_variables.R
@@ -1,50 +1,51 @@
-#' Define variables for ecocomDP tables
+#' Define ecocomDP variables
 #'
 #' @description  
-#'     Variables of ecocomDP are stored as categorical variables in long format.
-#'     Use this function to identify unique variables and assign definitions 
-#'     and any associated units.
+#'     This function identifies unique variables listed in the ecocomDP tables
+#'     and writes them to tables for you to provide definitions and units for.
 #'
-#' @usage define_variables(path, delimiter)
-#' 
-#'     Run this function to define variables prior to creating EML with 
-#'     \code{make_eml}.
+#' @usage define_variables(data.path, sep)
 #'
-#' @param path 
+#' @param data.path 
 #'     A path to the dataset working directory containing ecocomDP tables.
-#'     
-#' @param delimiter
-#'     The field delimiter of ecocomDP tables. Can be comma or tab delimited 
-#'     (i.e. "," or "\t")
+#' @param sep
+#'     The field separator string. Values within each row of ecocomDP tables
+#'     are separated by this string. Valid options are "," or "\t".
 #'
 #' @return 
-#'     A tab delimited UTF-8 file in the ecocomDP working directory titled 
-#'     \emph{variables_tablename.txt} containing unique values 
-#'     of attributes of class "categorical" which is translated and written 
-#'     to EML with \code{make_eml}.
+#'     A tab delimited UTF-8 file in the dataset working directory titled 
+#'     \emph{ecocomDPtablename_variables.txt} with fields:
+#'     \itemize{
+#'         \item \strong{attributeName} Name of the variable field from the
+#'         an ecocomDP table.
+#'         \item \strong{code} Unique variable names detected in an ecocomDP 
+#'         table.
+#'         \item \strong{definition} A field for users to supply definitions 
+#'         for variables listed in the code field.
+#'         \item \strong{units} A field for users to supply the units for each 
+#'         defined variable.
+#'     }
 #'     
 #' @details 
-#'     This function overwrites any 
-#'     \emph{variables_tablename.txt} files you have created in the ecocomDP 
-#'     working directory. To prevent overwriting of these files, temporarily 
-#'     move them out of the working directory.
+#'     Run this function after you have created and validated ecocomDP tables but
+#'     prior to create EML with \code{make_eml}. 
+#' 
+#'     Variables of ecocomDP tables are stored in long format. Use 
+#'     \code{define_variables} to identify unique variables and assign 
+#'     definitions and associated units.
 #'     
-#'     This function identifies unique variables listed in the ecocomDP tables
-#'     and writes them to a table (e.g. \emph{taxon_ancillary_variables}) for 
-#'     you to define. Do not define categorical variables with empty
-#'     field contents. Delete these rows from this file.
-#'     
-#'     When defining categorical variables with unit values, refer to the 
-#'     standard unit dictionary "name" column. The unit dictionary will be 
-#'     automatically opened when running this function. Enter the unit name in 
-#'     the unit column of the categorical variables table. Note these values 
-#'     are case sensitive. If you cannot find a unit in the dictionary, enter 
-#'     the unit in the tab delimited UTF-8 formatted file 
-#'     \emph{custom_units.txt} that has been automatically imported into your
-#'     working directory. Valid custom units must be convertible to SI Units 
-#'     (i.e. International System of Units). If it cannot be converted to SI 
-#'     then list it units column as "dimensionless". To create a custom unit 
-#'     define the:
+#'     Refer to the standard unit dictionary when selecting units. Open the 
+#'     standard unit dictionary by calling \code{view_unit_dictionary}. Search 
+#'     for a unit and enter the value listed in the \emph{name} field of the 
+#'     dictionary into the \emph{units} field of 
+#'     \emph{ecocomDPtablename_variables.txt}. NOTE these values are case 
+#'     sensitive. If you cannot find the unit you are looking for, enter the 
+#'     unit in the tab delimited UTF-8 formatted file \emph{custom_units.txt} 
+#'     that is imported into your dataset working directory when calling 
+#'     \code{import_templates}. Valid custom units must be convertible to SI 
+#'     Units (i.e. International System of Units). If it cannot be converted to
+#'     SI then list it in the units field as "dimensionless". To create a 
+#'     custom unit define the:
 #'     \itemize{
 #'         \item \strong{id} This is equivalent to the unit name. Reference 
 #'         the standard unit dictionary formatting
@@ -57,15 +58,18 @@
 #'         Reference the dictionary for examples.
 #'         }
 #'
+#' @seealso \code{\link{import_templates}} to import ecocomDP templates.
+#' @seealso \code{\link{make_eml}} to make EML for an ecocomDP.
+#'
 #' @export
 #'
 
 
-define_variables <- function(path, delimiter) {
+define_variables <- function(data.path, sep) {
   
   # Check arguments
   
-  if (missing(path)){
+  if (missing(data.path)){
     stop("Specify path to dataset working directory.")
   }
   
@@ -73,7 +77,7 @@ define_variables <- function(path, delimiter) {
   
   table_patterns <- c("observation\\b", "event\\b", "sampling_location_ancillary\\b", "taxon_ancillary\\b", "summary\\b", "sampling_location\\b", "taxon\\b")
   table_names <- c("observation", "event", "sampling_location_ancillary", "taxon_ancillary", "summary", "sampling_location", "taxon")
-  dir_files <- list.files(path)
+  dir_files <- list.files(data.path)
   table_names_found <- list()
   tables_found <- list()
   for (i in 1:length(table_patterns)){
@@ -96,17 +100,17 @@ define_variables <- function(path, delimiter) {
     
     print("Custom units template copied to working directory.")
     
-    write_catvars <- function(tables_found, delimiter, table_names){
+    write_catvars <- function(tables_found, sep, table_names){
       tables_out <- list()
       for (i in 1:length(tables_found)){
         if ((table_names[i] == "observation")|(table_names[i] == "event")|(table_names[i] == "sampling_location_ancillary")|(table_names[i] == "taxon_ancillary")){
           print(paste("Reading", tables_found[i]))
-          data_in <- read.table(paste(path,
+          data_in <- read.table(paste(data.path,
                                       "/",
                                       tables_found[i],
                                       sep = ""),
                                 header = T,
-                                sep = delimiter,
+                                sep = sep,
                                 as.is = T,
                                 na.strings = "NA")
           # Build the catvars table
@@ -122,7 +126,7 @@ define_variables <- function(path, delimiter) {
           # Write catvars table
           print(paste("Writing ", "variables_", table_names[i], ".txt", sep = ""))
           write.table(catvars,
-                      paste(path,
+                      paste(data.path,
                             "/",
                             table_names[i],
                             "_variables",
@@ -136,7 +140,7 @@ define_variables <- function(path, delimiter) {
         }
       }
     }
-    write_catvars(tables_found, delimiter, table_names)
+    write_catvars(tables_found, sep, table_names)
   }
   # Prompt the user to manually edit the catvars file and custom unit files.
   tables_out <- unlist(tables_out)
