f8de14635c404886af3880ea98e05aae476393cb
Colin Smith <colin.smith@wisc.edu>
2018-03-07 09:48:03 -0600

    Remove regexpr from literal text

--- R/compile_attributes.R
+++ R/compile_attributes.R
@@ -26,7 +26,7 @@ compile_attributes <- function(path, delimiter){
   # Set parameters ------------------------------------------------------------
   
   table_patterns <- c("observation\\b", "observation_ancillary\\b", "location_ancillary\\b", "taxon_ancillary\\b", "dataset_summary\\b", "location\\b", "taxon\\b", "variable_mappings\\b")
-  table_names <- c("observation", "observation_ancillary", "location_ancillary", "taxon_ancillary", "dataset_summary", "location", "taxon", "variable_mappings\\b")
+  table_names <- c("observation", "observation_ancillary", "location_ancillary", "taxon_ancillary", "dataset_summary", "location", "taxon", "variable_mappings")
   dir_files <- list.files(path)
   table_names_found <- list()
   tables_found <- list()
