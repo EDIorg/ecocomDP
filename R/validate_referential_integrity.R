2a079685cda55bd8ff83167cdc630e3642b8155b
Colin Smith <colin.smith@wisc.edu>
2018-08-31 18:03:19 -0500

    Create and test a function to validate referential integrity of L1 tables.

--- R/validate_referential_integrity.R
+++ R/validate_referential_integrity.R
@@ -21,7 +21,7 @@
 #'
 
 
-validate_composite_keys <- function(tables, data.path, criteria) {
+validate_referential_integrity <- function(tables, data.path, criteria) {
   
   # Check inputs --------------------------------------------------------------
   
@@ -59,13 +59,14 @@ validate_composite_keys <- function(tables, data.path, criteria) {
   #  for each foreign_key+table pair do the unique_values match unique primary_key+table pairs? if not, error foreign keys in tableX missing primary keys
   #
   
-  # Distill foreign keys + tables + values
+  # Get unique foreign key values ---------------------------------------------
   
   x <- unique.data.frame(criteria[(criteria$foreign_key == 'yes') & (!is.na(criteria$foreign_key)), 
-                   c('table', 'column')])
-  values <- vector('list', length(x$table))
+                                  c('table', 'column')])
   
-  get_values <- function(key, table.name, data.path){
+  foreign_key_tables <- x$table
+  
+  get_foreign_keys <- function(key, table.name, data.path){
     dir_tables <- list.files(data.path)
     table_pattern <- paste0(table.name, "\\b")
     use_i <- str_detect(dir_tables, table_pattern)
@@ -88,10 +89,117 @@ validate_composite_keys <- function(tables, data.path, criteria) {
     }
     
   }
-  values <- mapply(get_values, key = x$column, table.name = x$table, data.path = data.path)
-  # ----------------------
+  foreign_key_values <- mapply(get_foreign_keys, key = x$column, table.name = x$table, data.path = data.path)
+  
+  # Get unique primary key values ---------------------------------------------
+  
+  x <- unique.data.frame(criteria[(criteria$primary_key == 'yes') & (!is.na(criteria$primary_key)), 
+                                  c('table', 'column')])
+  
+  primary_key_tables <- x$table
+  
+  get_primary_keys <- function(key, table.name, data.path){
+    dir_tables <- list.files(data.path)
+    table_pattern <- paste0(table.name, "\\b")
+    use_i <- str_detect(dir_tables, table_pattern)
+    if (sum(use_i) > 0){
+      os <- detect_os()
+      sep <- detect_delimeter(path = data.path,
+                              data.files = dir_tables[use_i],
+                              os = os)
+      data_in <- read.table(paste0(data.path, "/", dir_tables[use_i]),
+                            header = T,
+                            sep = sep,
+                            as.is = T,
+                            quote = "\"",
+                            comment.char = "")
+      uni_values <- unique(data_in[ , key])
+      uni_values
+    } else {
+      uni_values <- NULL
+      uni_values
+    }
+    
+  }
+  primary_key_values <- mapply(get_primary_keys, key = x$column, table.name = x$table, data.path = data.path)
+  
+  # Foreign keys should have a matching primary key ---------------------------
+  
+  is_foreign_key <- function(fk.tables, fk.values, pk.values){
+    
+    fk.names <- names(fk.values)
+    pk.names <- names(pk.values)
+    
+    # Error if foreign key names don't match primary key names?
+    
+    use_i <- match(fk.names[fk.names != 'parent_location_id'], pk.names)
+    if (sum(is.na(use_i)) > 0){
+      stop(paste0("\n",
+                  'These foreign keys:\n',
+                  paste(fk.names(is.na(use_i)), collapse = ', '),
+                  '\nare missing primary keys'
+                  )
+           )
+    }
+    
+    # Validate parent_location_id
+    
+    if (sum(fk.names == 'parent_location_id') > 0){
+      fk_val <- fk.values[['parent_location_id']]
+      fk_val <- fk_val[!is.na(fk_val)] # Remove NA (NAs are allowed)
+      pk_val <- pk.values[['location_id']]
+      use_i <- is.na(match(fk_val, pk_val))
+      if (sum(use_i) > 0){
+        stop(paste0('\nThe foreign key "parent_location_id"',
+                    '\ncontains values that cannot be found in the primary key "location_id", specifically\n',
+                    paste(fk_val[use_i], collapse = ', ')
+                    )
+             )
+      }
+      fk.tables <- fk.tables[fk.tables != 'location']
+      fk.values <- fk.values[names(fk.values) != 'parent_location_id']
+    }
+    
+    # Validate other columns
+    
+    test <- function(fk.table, fk.name, fk.value, pk.value){
+      use_i <- match(fk.value, pk.value)
+      if (sum(is.na(use_i)) > 0){
+        stop(paste0('\nThe foreign key:\n',
+                    fk.name,
+                    '\nin this table:\n',
+                    fk.table,
+                    '\ncontains values that do not have a primary key match, namely:\n',
+                    paste(fk.val[is.na(use_i)], collapse = ', ')
+                    )
+             )
+      }
+    }
+    output <- test(fk.table, fk.name, fk.value, pk.value)
+    fk.tables
+    fk.names
+    fk.values
+    pk.values
+    mapply()
+    
+    
+    
+    #
+
+    
+    
+    
+  }
   
   
+  
+  
+  
+  
+  
+  
+  
+  # ----------------------------
   
   # Validation parameters -----------------------------------------------------
   
