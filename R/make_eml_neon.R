2af8c7e23cd3921ce333963cd62eba7215d522fe
clnsmth <colin.smith@wisc.edu>
2018-09-13 17:05:06 -0400

    Develop function to create EML for ecocomDP created from NEON data product

--- R/make_eml_neon.R
+++ R/make_eml_neon.R
@@ -12,6 +12,9 @@
 #'     (list of data frames) The ecocomDP tables for the NEON data product.
 #'     Each item of the list must be named after the ecocomDP tables (e.g.
 #'     observation, taxon, dataset_summary, etc.).
+#' @param protocols
+#'     (character) Vector of character strings listing the NEON sampling 
+#'     protocols.
 #' @param dp.id 
 #'     (character) NEON data product ID
 #'
@@ -24,6 +27,7 @@
 make_eml_neon <- function(
   eml,
   x,
+  protocols,
   dp.id
   ){
   
@@ -42,6 +46,12 @@ make_eml_neon <- function(
     ' at.*',
     ' (Reformatted to ecocomDP Design Pattern)'
     )
+  
+  short_name <- stringr::str_replace(
+    title,
+    ' at.*',
+    ' (Reformatted to ecocomDP Design Pattern)'
+  )
   
   title <- new(
     "title",
@@ -67,6 +77,12 @@ make_eml_neon <- function(
   abstract <- as(set_TextType(text = lns), "abstract")
   
   eml@dataset@abstract <- abstract
+  
+  # Edit short name -----------------------------------------------------------
+  
+  message('<shortName')
+  
+  eml@dataset@shortName <- short_name
   
   # Edit publication date -----------------------------------------------------
   
@@ -152,8 +168,6 @@ make_eml_neon <- function(
   
   dates <- lubridate::ymd_hm(x$observation$observation_datetime)
   
-  eml@dataset@coverage@temporalCoverage <- as()
-  
   temp <- set_coverage(
     begin = substr(floor_date(min(dates), unit = 'day'), 1, 10),
     end = substr(ceiling_date(max(dates), unit = 'day'), 1, 10)
@@ -195,10 +209,222 @@ make_eml_neon <- function(
     'ListOftaxonomicCoverage'
   )
   
-  #
   # Edit methods --------------------------------------------------------------
   
-  #
+  message('Add methods')
+  lns <- paste0(
+    'These NEON sampling protocols were used in creation of this dataset: ',
+    paste(protocols, collapse = ', '),
+    'Access these protocols at http://data.neonscience.org/documents'
+    )
+  ms <- as(lns, 'methodStep')
+  eml@dataset@methods@methodStep <- as(list(ms), 'ListOfmethodStep')
+  
+  # Edit attributes -----------------------------------------------------------
+
+  # Compile attributes
+  
+  attributes_in <- compile_attributes_neon(x)
+  data_tables_stored <- list()
+  datetime.format <- 'YYYY-MM-DDThh:mmZ' #!!! This may vary with NEON data product
+  
+  for (i in 1:length(attributes_in[[1]])){
+    use_i <- attributes_in[[1]][[i]]$columnClasses == "Date"
+    attributes_in[[1]][[i]]$formatString[use_i] <- datetime.format
+  }
+  
+  # Read table_descriptions.txt
+  
+  table_descriptions <- read.table(
+    system.file('table_descriptions.txt',
+                package = 'ecocomDP'
+    ),
+    header = T,
+    sep = "\t",
+    as.is = T,
+    na.strings = "NA",
+    colClasses = rep("character", 2)
+  )
+  
+  for (i in 1:length(x)){
+    
+    message(paste(
+      "Adding",
+      names(x)[i],
+      "<dataTable>"))
+    
+    attributes <- attributes_in[[1]][[i]]
+    
+    # Read data table
+    
+    df_table <- as.data.frame(x[[i]])
+    
+    # Read catvars file
+    # !!!This should be extracted from the NEON EML and will vary by data product
+    
+    # catvar <- grep(paste(table_names[i], "_variables.txt", sep = ""), dir_files, value = T)
+    
+    if ((dp.id == 'DP1.20120.001') & (names(x)[i] == 'observation')){
+      
+      catvars <- data.frame(
+        attributeName = c('variable_name'),
+        code = c('density'),
+        definition = c('Count per square meter.'),
+        stringsAsFactors = F
+      )
+      
+      # Clean up
+      
+      for (j in 1:dim(catvars)[2]){
+        catvars[ ,j] <- as.character(catvars[ ,j])
+      }
+      
+      non_blank_rows <- nrow(catvars) - sum(catvars$attributeName == "")
+      catvars <- catvars[1:non_blank_rows, 1:3]
+      
+      # Clean extraneous white spaces from catvars tables
+      
+      if (dim(catvars)[1] != 0){
+        for (j in 1:ncol(catvars)){
+          if (class(catvars[ ,j]) == "character" ||
+              (class(catvars[ ,j]) == "factor")){
+            catvars[ ,j] <- trimws(catvars[ ,j])
+          }
+        }
+      }
+      
+      # Clean extraneous white spaces from attributes
+      
+      for (j in 1:ncol(attributes)){
+        if (class(attributes[ ,j]) == "character" ||
+            (class(attributes[ ,j]) == "factor")){
+          attributes[ ,j] <- trimws(attributes[ ,j])
+        }
+      }
+      
+      # Get the column classes into a vector
+      
+      col_classes <- attributes[ ,"columnClasses"]
+      
+      # Create the attributeList element
+      
+      attributeList <- set_attributes(attributes,
+                                      factors = catvars,
+                                      col_classes = col_classes)
+      
+    } else {
+      
+      for (j in 1:ncol(attributes)){
+        if (class(attributes[ ,j]) == "character" ||
+            (class(attributes[ ,j]) == "categorical")){
+          attributes[ ,j] <- trimws(attributes[ ,j])
+        }
+      }
+      
+      # Get the column classes into a vector
+      
+      col_classes <- attributes[ ,"columnClasses"]
+      
+      # !!! This code should be replace by something more targeted
+      col_classes[col_classes == "factor"] <- "character"
+      # !!!
+      
+      # Create the attributeList element
+      
+      attributeList <- set_attributes(attributes,
+                                      col_classes = col_classes)
+      
+      
+    }
+    
+
+    # Set physical
+    
+    physical <- set_physical(names(x)[i],
+                             numHeaderLines = "1",
+                             recordDelimiter = "\\r\\n", # !!!This information is not accurate
+                             attributeOrientation = "column",
+                             fieldDelimiter = "\\t",
+                             quoteCharacter = "\"")
+    
+    
+    
+    
+    
+    
+    physical@size <- new("size",
+                         unit = "byte",
+                         as.character(
+                           object.size(x[[i]])
+                           )
+                         )
+    
+    # Get number of records
+    
+    number_of_records <- as.character(dim(df_table)[1])
+    
+    # Pull together information for the data table
+    
+    data_table <- new("dataTable",
+                      entityName = names(x)[i],
+                      entityDescription = table_descriptions$description[match(names(x)[i], table_descriptions$table_name)],
+                      physical = physical,
+                      attributeList = attributeList,
+                      numberOfRecords = number_of_records)
+    
+    data_tables_stored[[i]] <- data_table
+    
+  }
+  
+  # Compile data tables
+  
+  eml@dataset@dataTable <- new("ListOfdataTable",
+                                  data_tables_stored)
+  
+  # Build EML -----------------------------------------------------------------
+  
+  eml <- new("eml",
+             schemaLocation = "eml://ecoinformatics.org/eml-2.1.1  http://nis.lternet.edu/schemas/EML/eml-2.1.1/eml.xsd",
+             packageId = 'This data product is synthesized on demand from the NEON data portal and does not have a package ID.',
+             system = 'These data are not stored in a repository.',
+             access = eml@access,
+             dataset = eml@dataset)
+  
+  # Validate EML --------------------------------------------------------------
+  
+  # message("Validating EML")
+  # 
+  # validation_result <- eml_validate(eml)
+  # 
+  # if (validation_result == "TRUE"){
+  #   
+  #   message("EML passed validation!")
+  #   
+  # } else {
+  #   
+  #   message("EML validaton failed. See warnings for details.")
+  #   
+  # }
+  
+  write_eml(eml, '/Users/csmith/Documents/EDI/datasets/ecocomDP_tests/data.xml')
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
