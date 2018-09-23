1493fcff193aa201ae64b0699cc10ec82384ef1c
clnsmth <colin.smith@wisc.edu>
2018-09-23 09:42:56 -0400

    Update documentation of aggregate_ecocomDP.R

--- R/aggregate_ecocomDP.R
+++ R/aggregate_ecocomDP.R
@@ -1,46 +1,27 @@
 #' aggregate_ecocomDP
 #'
 #' @description  
-#'     View list of all available ecocomDP with high level metadata.
+#'     Aggregate one or more ecocomDP.
 #'
-#' @usage view_all_ecocomDP()
+#' @usage aggregate_ecocomDP(package.ids)
+#'     
+#' @param package.ids
+#'     (character) One or more data package IDs listed in the data frame created by 
+#'     `list_all_ecocomDP`. EDI data package IDs are of the form 
+#'     'edi.package_number.revision_number' (e.g. edi.100.2). NEON data package
+#'     IDs are of the form 'data_product.identifier.revision_number' (e.g.
+#'     'DP1.20120.001'). NOTE: Previous data package revisions of ecocomDP 
+#'     packages are valid entries.
 #'     
 #' @return 
-#'     A data frame with fields:
-#'     \itemize{
-#'         \item package_id: Data package ID
-#'         \item title: Data package title
-#'         \item original_package_id: The parent data package ID
-#'         \item length_of_survey_years: The number of years surveyed
-#'         \item number_of_years_sampled: Number of years sampled
-#'         \item std_dev_interval_betw_years: Standard deviation interval between years
-#'         \item max_num_taxa: Number of unique taxa observed
-#'         \item geo_extent_bounding_box_m2: Area (square meters) over which samples were taken.
-#'     }
+#'     A single ecocomDP containing all the content of the individual ecocomDP
+#'     listed in the argument "package.ids".
 #'         
 #' @export
 #'
 
 aggregate_ecocomDP <- function(){
   
-  # - Search EDI Data Repository for ‘ecocomDP’
-  # - Get:
-  #   - Package identifiers of listed data packages
-  # - Data package titles
-  # - Short descriptions of data packages
-  # - dataset_summary table
-  # - URLs to data package landing pages. Can these be hyperlinked
-  # - store in named list of single line data frames
-  # 
-  # - Get: 
-  #   - data product IDs from neon_data_products_for_ecocomDP.txt listed in /inst 
-  # - dataset_summary.txt for each data product ID
-  # - Create function call to open locally stored HTML (so user can copy and paste into RStudio)
-  # - store in named list of single line data frames
-  # 
-  # - Convert 2 lists into a data frame
-  # - Create leading column as ecocomDP_id
-  # - Create data frame and export to local workspace from function (this will be used by view_metadata.R).
-  # - View data frame in RStudio
+  
   
 }
