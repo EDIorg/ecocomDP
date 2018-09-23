0c4d1e50285930a6361b5c4101fee243a95f248c
clnsmth <colin.smith@wisc.edu>
2018-09-23 09:25:06 -0400

    Complete development of view_all_ecocomDP.R

--- R/view_all_ecocomDP.R
+++ R/view_all_ecocomDP.R
@@ -25,6 +25,8 @@ view_all_ecocomDP <- function(){
 
   # Query the EDI Data Repository ---------------------------------------------
   
+  message('Searching EDI ...')
+  
   # Get package IDs and titles
   
   query <- httr::GET('https://pasta.lternet.edu/package/search/eml?q=keyword:ecocomDP&fl=packageid,title&rows=1000')
@@ -45,25 +47,168 @@ view_all_ecocomDP <- function(){
     )
   }
   
+  # Get package EML
+  
+  get_eml <- function(package.id){
+    message(paste0('found ', package.id))
+    pkg_prts <- unlist(strsplit(package.id, split = ".", fixed = T))
+    metadata <- XML::xmlParse(paste0("http://pasta.lternet.edu/package/metadata/eml",
+                                    "/",
+                                    pkg_prts[1],
+                                    "/",
+                                    pkg_prts[2],
+                                    "/",
+                                    pkg_prts[3]))
+    metadata
+  }
+  eml <- lapply(pkg_ids, FUN = get_eml)
+  
+  # Get dataset observation tables
+  
+  get_dataset_observation <- function(package.id, eml){
+    entity_names <- unlist(
+      xmlApply(eml["//dataset/dataTable/entityName"], 
+               xmlValue)
+    )
+    entity_delimiters <- unlist(
+      xmlApply(eml["//dataset/dataTable/physical/dataFormat/textFormat/simpleDelimited/fieldDelimiter"], 
+               xmlValue)
+    )
+    entity_urls <- unlist(
+      xmlApply(eml["//dataset/dataTable/physical/distribution/online/url"], 
+               xmlValue)
+    )
+    if (length(grep('dataset_summary', entity_names)) != 0){
+      file_name <- entity_names[grep('dataset_summary', entity_names)]
+      file_delimiter <- entity_delimiters[grep('dataset_summary', entity_names)]
+      file_url <- entity_urls[grep('dataset_summary', entity_names)]
+      
+      
+      if (file_delimiter == ","){
+        file_data <-read.table(sub("^https", "http", file_url),
+                                      header = T, 
+                                      sep=",",
+                                      as.is = T,
+                                      fill = T)
+      } else if (file_delimiter == "\\t"){
+        file_data <-read.table(sub("^https", "http", file_url),
+                                      header = T, 
+                                      sep="\t",
+                                      as.is = T,
+                                      fill = T)
+      }
+    } else {
+      file_data <- NULL
+    }
+    list(file_data)
+  }
+  message('summarizing EDI data')
+  edi_data <- mapply(get_dataset_observation, pkg_ids, eml)
+  
+  message('... done.')
+  
+  # Get available NEON data products  -----------------------------------------
+  
+  message('Searching NEON ...')
+  
+  # Get package IDs and titles
+  
+  criteria <- suppressWarnings(read.table(
+    system.file('neon_data_products_for_ecocomDP.txt', package = 'ecocomDP'),
+    header = T,
+    sep = "\t",
+    as.is = T,
+    na.strings = "NA"))
+  
+  neon_pkg_ids <- criteria$data_product_id[is.na(criteria$tables_to_download)]
+  neon_pkg_titles <- criteria$title[is.na(criteria$tables_to_download)]
+  
+  # Get dataset observation tables
+  
+  get_dataset_observation_neon <- function(package.id){
+    message(paste0('found ', package.id))
+    use_i <- grep(
+      pattern = paste0(
+        package.id,
+        '_neon_dataset_summary'
+        ),
+      x = list.files(
+        system.file(
+          package = 'ecocomDP'
+          )
+        )
+      )
     
+    if ((length(use_i) != 0)){
+      file_delimiter <- detect_delimeter(
+        path = file.path(system.file(package = 'ecocomDP')),
+        data.files = list.files(system.file(package = 'ecocomDP'))[use_i],
+        os = detect_os())
+      if (file_delimiter == ","){
+        file_data <-read.table(paste0(file.path(system.file(package = 'ecocomDP')),
+                                      '/',
+                                      list.files(system.file(package = 'ecocomDP'))[use_i]),
+                               header = T, 
+                               sep=",",
+                               as.is = T,
+                               fill = T)
+      } else if (file_delimiter == "\t"){
+        file_data <-read.table(paste0(file.path(system.file(package = 'ecocomDP')),
+                                      '/',
+                                      list.files(system.file(package = 'ecocomDP'))[use_i]),
+                               header = T, 
+                               sep="\t",
+                               as.is = T,
+                               fill = T)
+      }
+    }
+    file_data
+  }
+  neon_data <- lapply(package.id, get_dataset_observation_neon)
+  message('summarizing NEON data')
   
-  # - Get:
-  #   - Package identifiers of listed data packages
-  # - Data package titles
-  # - Short descriptions of data packages
-  # - dataset_summary table
-  # - URLs to data package landing pages. Can these be hyperlinked
-  # - store in named list of single line data frames
-  # 
-  # - Get: 
-  #   - data product IDs from neon_data_products_for_ecocomDP.txt listed in /inst 
-  # - dataset_summary.txt for each data product ID
-  # - Create function call to open locally stored HTML (so user can copy and paste into RStudio)
-  # - store in named list of single line data frames
-  # 
-  # - Convert 2 lists into a data frame
-  # - Create leading column as ecocomDP_id
-  # - Create data frame and export to local workspace from function (this will be used by view_metadata.R).
-  # - View data frame in RStudio
+  message('... done.')
   
+  # Compile EDI and NEON data then view ---------------------------------------
+  
+  message('Compiling EDI and NEON data ...')
+  
+  rows <- length(edi_data) + length(neon_data)
+  data_out <- data.frame(
+    package_id = character(rows),
+    title = character(rows),
+    original_package_id = character(rows),
+    length_of_survey_years = numeric(rows),
+    number_of_years_sampled = numeric(rows),
+    std_dev_interval_betw_years = numeric(rows),
+    max_num_taxa = numeric(rows),
+    geo_extent_bounding_box_m2 = numeric(rows),
+    stringsAsFactors = F)
+  
+  for (i in 1:length(edi_data)){
+    data_out[i, 'package_id'] <- names(edi_data[i])
+    data_out[i, 'title'] <- pkg_titles[i]
+    data_out[i, 'original_package_id'] <- edi_data[[i]][1, 'original_package_id']
+    data_out[i, 'length_of_survey_years'] <- edi_data[[i]][1, 'length_of_survey_years']
+    data_out[i, 'number_of_years_sampled'] <- edi_data[[i]][1, 'number_of_years_sampled']
+    data_out[i, 'std_dev_interval_betw_years'] <- edi_data[[i]][1, 'std_dev_interval_betw_years']
+    data_out[i, 'max_num_taxa'] <- edi_data[[i]][1, 'max_num_taxa']
+    data_out[i, 'geo_extent_bounding_box_m2'] <- edi_data[[i]][1, 'geo_extent_bounding_box_m2']
+  }
+  
+  j <- i + 1
+  for (i in 1:length(neon_data)){
+    data_out[j, 'package_id'] <- neon_pkg_ids[i]
+    data_out[j, 'title'] <- neon_pkg_titles[i]
+    data_out[j, 'original_package_id'] <- neon_data[[i]][1, 'original_package_id']
+    data_out[j, 'length_of_survey_years'] <- neon_data[[i]][1, 'length_of_survey_years']
+    data_out[j, 'number_of_years_sampled'] <- neon_data[[i]][1, 'number_of_years_sampled']
+    data_out[j, 'std_dev_interval_betw_years'] <- neon_data[[i]][1, 'std_dev_interval_betw_years']
+    data_out[j, 'max_num_taxa'] <- neon_data[[i]][1, 'max_num_taxa']
+    data_out[j, 'geo_extent_bounding_box_m2'] <- neon_data[[i]][1, 'geo_extent_bounding_box_m2']
+  }
+  
+  View(data_out)
+  data_out
+ 
 }
