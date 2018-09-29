24512123f5684b3fa63abf98cd3cfb55319b1e51
clnsmth <colin.smith@wisc.edu>
2018-09-29 13:43:26 -0500

    Create an example workflow for the data user

---
+++ documentation/examples/user_workflow.R
@@ -0,0 +1,22 @@
+# An example workflow for the data user
+
+# Get a list of all ecocomDPs available from EDI and NEON
+ecocomDP_list <- view_all_ecocomDP()
+
+# Browse data package landing pages to identify ecocomDPs of interest
+view_landing_page('edi.124.3')
+view_landing_page('edi.191.2')
+view_landing_page('knb-lter-ntl.345.3')
+view_landing_page('DP1.20120.001')
+
+# Aggregate select ecocomDPs
+data <- aggregate_ecocomDP(
+  package.ids = c('edi.124.3', 
+                  'edi.191.2', 
+                  'knb-lter-ntl.345.3', 
+                  'DP1.20120.001'),
+  path = '/Users/csmith/Documents/ecocomDP_export'
+  )
+
+
+
