038cde47a97df705d6b464f62e49eb9af7ab146c
Colin Smith <colin.smith@wisc.edu>
2020-05-07 13:18:48 -0700

    Begin unpacking nested site lat, lon, and elv

--- R/manipulate_tables.R
+++ R/manipulate_tables.R
@@ -26,9 +26,9 @@
 #' 
 long2wide_obs_loc_tax <- function(dt_obs, dt_loc, dt_tax) {
   
-  # Expand location table
-  # TODO: Incorporate latitude, longitude, and elevation (see notes:
-  # https://docs.google.com/document/d/1-s0EGF1TgQneVOaQFQBlrUOmnfOAs9M1mGk-IMVHYnQ/edit)
+  # Expand location table -----------------------------------------------------
+  
+  # Rewrite names of locations to indicate their nested arrangements
   loc_name_combined <- rep(NA_character_, nrow(dt_loc))
   for (i in 1:length(dt_loc$location_id)) {
     if (!is.na(dt_loc$parent_location_id[i])) {
@@ -49,6 +49,37 @@ long2wide_obs_loc_tax <- function(dt_obs, dt_loc, dt_tax) {
         collapse = ".")
     }
   }
+  
+  # Expand latitude and longitude (and possibly elevation when it exists) from
+  # parent locations to children if the children don't have these values.
+  # TODO: Incorporate latitude, longitude, and elevation (see notes:
+  # https://docs.google.com/document/d/1-s0EGF1TgQneVOaQFQBlrUOmnfOAs9M1mGk-IMVHYnQ/edit)
+  loc_lat_lon_elv <- data.frame(
+    lat = rep(NA_character_, nrow(dt_loc)),
+    lon = rep(NA_character_, nrow(dt_loc)),
+    elv = rep(NA_character_, nrow(dt_loc)),
+    stringsAsFactors = FALSE)
+  for (i in 1:length(dt_loc$location_id)) {
+    if (!is.na(dt_loc$parent_location_id[i])) {
+      id_out <- dt_loc$location_id[i]
+      id <- dt_loc$location_id[i]
+      cont <- TRUE
+      while (isTRUE(cont)) {
+        if (!is.na(dt_loc$parent_location_id[id == dt_loc$location_id])) {
+          id_out[length(id_out) + 1] <- dt_loc$parent_location_id[id == dt_loc$location_id]
+          id <- dt_loc$parent_location_id[id == dt_loc$location_id]
+        } else {
+          cont <- FALSE
+        }
+      }
+      loc_name_combined[i] <- paste(
+        dt_loc$location_name[
+          dt_loc$location_id %in% rev(id_out)], 
+        collapse = ".")
+    }
+  }
+  
+  # Create the expanded location table
   loc_name_combined[is.na(dt_loc$parent_location_id)] <- dt_loc$location_name[is.na(dt_loc$parent_location_id)]
   dt_loc_expanded <- dt_loc
   dt_loc_expanded$location_name <- loc_name_combined
