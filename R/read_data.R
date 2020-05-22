2f0f53eecb75d87e24954f4b67730974fea06d88
Colin Smith <colin.smith@wisc.edu>
2020-05-21 19:29:46 -0700

    Continue developing read_data()

--- R/read_data.R
+++ R/read_data.R
@@ -1,212 +1,159 @@
 #' Read ecocomDP data
 #'
 #' @param id
-#'     (character) Identifier (one or more) of ecocomDP datasets to read. IDs 
-#'     are listed in the "id" column of search results output from 
-#'     \code{search_data()}.
-#' @param site 
-#'     (character) For NEON data. Either the string "all", meaning 
-#'     all available sites, or a character vector of codes listed in the 
-#'     "sites" column of results output from \code{search_data()}. Defaults to 
-#'     "all".
-#' @param startdate
-#'     (character) For NEON data. Either NA, meaning all available dates, or a 
-#'     character vector in the form YYYY-MM, e.g. 2017-01. Defaults to NA.
-#' @param enddate 
-#'     (character) For NEON data. Either NA, meaning all available dates, or a 
-#'     character vector in the form YYYY-MM, e.g. 2017-01. Defaults to NA.
-#' @param check.size 
-#'     (logical) For NEON data. T or F, should the user approve the total file 
-#'     size before downloading? Defaults to T. When working in batch mode, or 
-#'     other non-interactive workflow, use check.size = F.
-#' @param nCores
-#'     (integer) For NEON data. The number of cores to parallelize the 
-#'     stacking procedure. By default it is set to a single core.
-#' @param forceParallel 
-#'     (logical) For NEON data. If the data volume to be processed does not 
-#'     meet minimum requirements to run in parallel, this overrides. Set to 
-#'     FALSE as default.
+#'     (character) Identifier of dataset to read. Identifiers are listed in the 
+#'     "id" column of the \code{search_data()} output.  If reading multiple 
+#'     datasets, then \code{id} should be a character vector of identifiers or 
+#'     a named list containing additional arguments to filter on (for NEON data 
+#'     only; see below for arguments and examples).
 #' @param path
-#'     (character; optional) Path to the directory in which the aggregated 
-#'     ecocomDP will be written.
+#'     (character) Path to the directory in which the data will be written file 
+#'     (one file per ecocomDP table).
+#' @param site 
+#'     (character; NEON data only) A character vector of site codes to filter 
+#'     data on. Sites are listed in the "sites" column of the 
+#'     \code{search_data()} output. Defaults to "all", meaning all sites.
+#' @param startdate
+#'     (character; NEON data only) Start date to filter on in the form YYYY-MM. 
+#'     Defaults to NA, meaning all available dates.
+#' @param enddate 
+#'     (character; NEON data only) End date to filter on in the form YYYY-MM. 
+#'     Defaults to NA, meaning all available dates.
+#' @param check.size 
+#'     (logical; NEON data only) Should the user approve the total file size 
+#'     before downloading? Defaults to TRUE.
+#' @param nCores
+#'     (integer; NEON data only) The number of cores to parallelize the 
+#'     stacking procedure. Defaults to 1.
+#' @param forceParallel 
+#'     (logical; NEON data only) If the data volume to be processed does not 
+#'     meet minimum requirements to run in parallel, this overrides. Defaults 
+#'     to FALSE.
+#'     
 #' @return
 #'     (list) A named list, including: 
-#' \item{metadata}{A list of information about the data.}
-#' \item{tables}{A list of data.frames following the ecocomDP format.}
+#'     \item{metadata}{A list of information about the data.}
+#'     \item{tables}{A list of data.frames following the ecocomDP format 
+#'     (\url{https://github.com/EDIorg/ecocomDP/blob/master/documentation/model/table_visualization.md})}
+#'     
 #' @details 
-#'     Each data source has a corresponding reader. Determine the source from 
-#'     the id and call the correct reader.
+#'     Validation checks are applied to each dataset ensuring they comply with 
+#'     the ecocomDP. A warning is issued when validation fails (please report 
+#'     these to \url{https://github.com/EDIorg/ecocomDP/issues}). Datasets that
+#'     pass validation are returned.
+#'     
+#'     Column classes are coerced to those defined in the ecocomDP 
+#'     specification.
+#'     
+#'     Reading multiple datasets raises the issue of referential integrity. 
+#'     This is addressed by adding the \code{id} to each table's primary key
+#'     (e.g. observation_id, event_id, etc.).
+#'     
 #' @export
+#' 
 #' @examples
+#' # Read a dataset
 #' d <- read_data("edi.193.3")
 #' d <- read_data("DP1.20166.001")
-read_data <- function(id, site = "all", startdate = NA, enddate = NA, 
-                      check.size = TRUE, nCores = 1, forceParallel = FALSE, 
-                      path) {
-  # TODO: Validate inputs
-  # TODO: Wrap map_neon_data_to_ecocomDP()
-  # TODO: Refactor inputs for multiple NEON datasets (supply as list?)
+#' 
+#' # Read a NEON dataset with filters
+#' d <- read_data(
+#'   id = "DP1.20166.001", 
+#'   site = c("MAYF", "PRIN"), 
+#'   startdate = "2016-1", 
+#'   enddate = "2018-11")
+#' 
+#' # Read multiple datasets and concatenate together
+#' d <- read_data(c("edi.193.3", "DP1.20166.001"))
+#' 
+#' # Read multiple datasets from EDI and NEON with NEON filters.
+#' d <- read_data(
+#'   id = list(
+#'     edi.193.3 = NULL,
+#'     DP1.20166.001 = list(
+#'       site = c("MAYF", "PRIN"),
+#'       startdate = "2016-1",
+#'       enddate = "2018-11",
+#'       check.size = FALSE)))
+#' 
+read_data <- function(
+  id, path, site = "all", startdate = NA, enddate = NA, check.size = TRUE, 
+  nCores = 1, forceParallel = FALSE) {
   
   message("Reading ", id)
   
-  # EDI -----------------------------------------------------------------------
+  # Parameterize --------------------------------------------------------------
   
-  if (stringr::str_detect(
+  # Get ecocomDP attributes for validation and coercion
+  attr_tbl <- data.table::fread(
+    system.file('validation_criteria.txt', package = 'ecocomDP'))
+  
+  # Identify data sources
+  edi_i <- stringr::str_detect(
     id, 
-    "(^knb-lter-[:alpha:]+\\.[:digit:]+\\.[:digit:]+)|(^[:alpha:]+\\.[:digit:]+\\.[:digit:]+)")) {
-    
-    # Get attributes of input tables ------------------------------------------
-    # Get the URL, file name, number of records, and field delimiter of each 
-    # table to be aggregated so the output tables may be initialized for better
-    # performance.
-    
-    # Get ecocomDP attributes
-    
-    attrs_ecocomDP <- suppressMessages(
-      readr::read_tsv(
-        system.file('validation_criteria.txt', package = 'ecocomDP')))
-    
-    tbls <- unique(attrs_ecocomDP$table)
-    
-    # Initialize attributes list
-    
-    tbl_attrs <- lapply(
-      seq_along(id),
-      function(x){
-        out <- lapply(
-          seq_along(vector('list', length(tbls))), 
-          function(x){
-            list(name = NULL, url = NULL, delimiter = NULL, nrecord = NULL)
-          }
-        )
-        names(out) <- tbls
-        out
-      }
-    )
-    names(tbl_attrs) <- id
-    
-    # Fill attributes list
-    
-    xpath <- c(
-      name = './/dataset/dataTable/physical/objectName',
-      url = './/dataset/dataTable/physical/distribution/online/url',
-      delimiter = './/dataset/dataTable/physical/dataFormat/textFormat/simpleDelimited/fieldDelimiter',
-      nrecord = './/dataset/dataTable/numberOfRecords')
-    
-    invisible(
-      lapply(
-        seq_along(tbl_attrs),
-        function(x){
-          eml <- suppressMessages(
-            EDIutils::api_read_metadata(names(tbl_attrs)[x]))
-          tblnames <- xml2::xml_text(xml2::xml_find_all(eml, xpath[['name']]))
-          pkg_id <- names(tbl_attrs)[x]
-          lapply(
-            seq_along(tbls),
-            function(x){
-              i <- stringr::str_detect(tblnames, paste0(tbls[x], '\\.[:alnum:]*$'))
-              if (any(i)){
-                tbl <- tbls[x]
-                lapply(
-                  seq_along(xpath),
-                  function(x){
-                    tbl_attrs[[pkg_id]][[tbl]][[x]] <<- xml2::xml_text(
-                      xml2::xml_find_all(eml, xpath[x]))[i]
-                  })
-              }
-            })
-        }))
-    
-    # Initialize output tables ------------------------------------------------
-    # Outputs are ecocomDP tables filled with NA_character_ of nrows equal to the 
-    # number of records of the inputs.
-    
-    out <- lapply(
-      seq_along(tbls),
-      function(x){
-        nrecord <- sum(as.numeric(unlist(purrr::map(
-          .x = tbl_attrs, 
-          list(tbls[x], 'nrecord')))))
-        cnames <- na.omit(attrs_ecocomDP$column[attrs_ecocomDP$table == tbls[x]])
-        out <- tibble::as_tibble(data.frame(matrix(NA_character_, 
-                                                   nrow = nrecord, 
-                                                   ncol = length(cnames)),
-                                            stringsAsFactors = FALSE))
-        colnames(out) <- cnames
-        out
-      }
-    )
-    names(out) <- tbls
-    
+    "(^knb-lter-[:alpha:]+\\.[:digit:]+\\.[:digit:]+)|(^[:alpha:]+\\.[:digit:]+\\.[:digit:]+)")
+  neon_i <- stringr::str_detect(id, "^DP1")
+
+  # Read from EDI -------------------------------------------------------------
+  # Read from the Environmental Data Initiative Data Repository
+  
+  if (any(edi_i)) {
+    edi_d <- lapply(id[edi_i], read_data_edi)
+    names(edi_d) <- id[edi_i]
+  }
+
+  # Read from NEON ------------------------------------------------------------
+  # Read from the National Ecological Observatory Network
+  
+  if (any(neon_i)) {
+    neon_d <- ecocomDP::map_neon_data_to_ecocomDP(
+      neon.data.product.id = id,
+      site = site,
+      startdate = startdate,
+      enddate = enddate,
+      check.size = check.size,
+      nCores = nCores,
+      forceParallel = forceParallel)
+    browser()
   }
   
-  # Fill output tables --------------------------------------------------------
-  # Sequentially fill output tables
+  # Apply validation checks ---------------------------------------------------
   
-  message('Creating tables:')
+  # Coerce column classes -----------------------------------------------------
+  # # Coerce column classes to ecocomDP specifications.
+  # # FIXME Harmonize date formats of different temporal resolutions
+  # 
+  # invisible(
+  #   lapply(
+  #     seq_along(out),
+  #     function(i){
+  #       use_tbl <- names(out[i])
+  #       if (!is.null(out[[use_tbl]])){
+  #         lapply(
+  #           seq_along(colnames(out[[use_tbl]])),
+  #           function(j){
+  #             use_col <- colnames(out[[use_tbl]])[[j]]
+  #             use_class <- attr_tbl$class[
+  #               ((attr_tbl$table == use_tbl) & 
+  #                  (!is.na(attr_tbl$column)) & 
+  #                  (attr_tbl$column == use_col))]
+  #             out[[use_tbl]][[use_col]] <<- col2class(
+  #               column = out[[use_tbl]][[use_col]],
+  #               class = use_class)
+  #           }
+  #         )
+  #       }
+  #     }
+  #   )
+  # )
   
-  invisible(
-    lapply(
-      seq_along(tbls),
-      function(i){
-        message(paste0('  ', tbls[i]))
-        counter <- 1
-        url <- unlist(purrr::map(.x = tbl_attrs, 
-                                 list(tbls[i], 'url')))
-        delim <- unlist(purrr::map(.x = tbl_attrs, 
-                                   list(tbls[i], 'delimiter')))
-        nrecord <- cumsum(unlist(purrr::map(.x = tbl_attrs, 
-                                            list(tbls[i], 'nrecord'))))
-        lapply(
-          seq_along(url),
-          function(j){
-            # Read file
-            if (delim[j] == ','){
-              d <- suppressWarnings(suppressMessages(readr::read_csv(
-                url[j], 
-                col_types = paste0(
-                  rep('c', 
-                      length(
-                        na.omit(
-                          attrs_ecocomDP$class[
-                            attrs_ecocomDP$table == tbls[i]]))), 
-                  collapse = ''))))
-            } else if (delim[j] == '\\t'){
-              d <- suppressWarnings(suppressMessages(readr::read_tsv(
-                url[j], 
-                col_types = paste0(
-                  rep('c', 
-                      length(
-                        na.omit(
-                          attrs_ecocomDP$class[
-                            attrs_ecocomDP$table == tbls[i]]))), 
-                  collapse = ''))))
-            }
-            # Create globally uniqe identifiers by appending package ID to 
-            # primary and foreign keys.
-            d <- dplyr::mutate_at(
-              d, 
-              .vars = na.omit(attrs_ecocomDP$column[
-                (attrs_ecocomDP$table == tbls[i]) & 
-                  (attrs_ecocomDP$globally_unique == 'yes')]),
-              .funs = function(x){
-                paste0(x, '_', names(url[j]))
-              })
-            # Clean up byproducts created with globally unique identifiers
-            if (tbls[i] == 'location'){
-              d$parent_location_id[
-                stringr::str_detect(d$parent_location_id, 'NA')] <- NA_character_
-            }
-            # Fill output tables
-            suppressWarnings(
-              out[[i]][counter:nrecord[j], 
-                       match(colnames(d), colnames(out[[i]]))] <<- d)
-            counter <<- sum(counter, nrecord[j])
-          }
-        )
-      }
-    )
-  )
+  # Combine sources -----------------------------------------------------------
+  # Combine results from different sources
+  
+  if (length(id) > 1) {
+    # Append package_id to primary keys to ensure referential integrity
+    
+  }
   
   # Coerce column classes -----------------------------------------------------
   # Coerce column classes to ecocomDP specifications.
@@ -222,10 +169,10 @@ read_data <- function(id, site = "all", startdate = NA, enddate = NA,
             seq_along(colnames(out[[use_tbl]])),
             function(j){
               use_col <- colnames(out[[use_tbl]])[[j]]
-              use_class <- attrs_ecocomDP$class[
-                ((attrs_ecocomDP$table == use_tbl) & 
-                   (!is.na(attrs_ecocomDP$column)) & 
-                   (attrs_ecocomDP$column == use_col))]
+              use_class <- attr_tbl$class[
+                ((attr_tbl$table == use_tbl) & 
+                   (!is.na(attr_tbl$column)) & 
+                   (attr_tbl$column == use_col))]
               out[[use_tbl]][[use_col]] <<- col2class(
                 column = out[[use_tbl]][[use_col]],
                 class = use_class)
@@ -235,29 +182,6 @@ read_data <- function(id, site = "all", startdate = NA, enddate = NA,
       }
     )
   )
-  
-  # NEON ----------------------------------------------------------------------
-  # Read from NEON
-  
-  # read_data <- function(id, site = "all", startdate = NA, enddate = NA, 
-  #                       check.size = TRUE, nCores = 1, forceParallel = FALSE, 
-  #                       path) {
-  
-  if (stringr::str_detect(id, "^DP1")) {
-    out_neon <- ecocomDP::map_neon_data_to_ecocomDP(
-      neon.data.product.id = id,
-      site = site,
-      startdate = startdate,
-      enddate = enddate,
-      check.size = check.size,
-      nCores = nCores,
-      forceParallel = FALSE)
-  }
-  
-  # Combine sources -----------------------------------------------------------
-  # Combine results from different sources
-  
-  
   
   # Return --------------------------------------------------------------------
   
@@ -327,4 +251,89 @@ col2class <- function(column, class){
     column <- as.character(column)
   }
   column
+}
+
+
+
+
+
+#' Read an ecocomDP dataset from EDI
+#'
+#' @param id
+#'     (character) Data package identifier with revision number
+#'
+#' @return
+#'     (list) Named list of data tables
+#'     
+read_data_edi <- function(id) {
+ 
+  # Get table metadata for reading
+  
+  tbl_attrs <- lapply(
+    seq_along(vector('list', length(unique(attr_tbl$table)))), 
+    function(x){
+      list(name = NULL, url = NULL, delimiter = NULL, nrecord = NULL)
+    })
+  names(tbl_attrs) <- unique(attr_tbl$table)
+  
+  xpath <- c(
+    name = './/dataset/dataTable/physical/objectName',
+    url = './/dataset/dataTable/physical/distribution/online/url',
+    delimiter = './/dataset/dataTable/physical/dataFormat/textFormat/simpleDelimited/fieldDelimiter',
+    nrecord = './/dataset/dataTable/numberOfRecords')
+  
+  eml <- suppressMessages(EDIutils::api_read_metadata(id))
+  invisible(
+    lapply(
+      names(tbl_attrs),
+      function(x) {
+        use_i <- stringr::str_detect(
+          xml2::xml_text(xml2::xml_find_all(eml, xpath[['name']])), 
+          paste0(x, '\\.[:alnum:]*$'))
+        if (any(use_i)) {
+          lapply(
+            names(xpath),
+            function(k){
+              tbl_attrs[[x]][[k]] <<- xml2::xml_text(
+                xml2::xml_find_all(eml, xpath[[k]]))[use_i]
+            })
+        } else {
+          tbl_attrs[[x]] <<- NULL
+        }
+      }))
+  
+  # Read tables
+  # Create globally uniqe identifiers by appending package ID to primary and 
+  # foreign keys.
+  
+  output <- lapply(
+    names(tbl_attrs),
+    function(x) {
+      d <- suppressMessages(
+        data.table::fread(
+          tbl_attrs[[x]]$url, 
+          sep = tbl_attrs[[x]]$delimiter))
+      # TODO: Add missing columns
+      
+      # # TODO: Move to join_data()
+      # d <- dplyr::mutate_at(
+      #   d, 
+      #   .vars = na.omit(attr_tbl$column[
+      #     (attr_tbl$table == x) & (attr_tbl$globally_unique == 'yes')]),
+      #   .funs = function(k){
+      #     paste0(k, '_', id)
+      #   })
+      
+      # Clean up byproducts created with globally unique identifiers
+      if (x == 'location') {
+        d$parent_location_id[
+          stringr::str_detect(d$parent_location_id, 'NA')] <- NA_character_
+      }
+      d
+    })
+  names(output) <- names(tbl_attrs)
+  list(
+    metadata = NULL,
+    tables = output)
+  
 }
