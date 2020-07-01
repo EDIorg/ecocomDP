644821b356ed90ed091b0a66e065a303aefee15e
Ruvi Esmeralda Jaimes <groovy@ruvis-mbp.attlocal.net>
2020-07-01 10:14:28 -0500

    package_id

--- R/map_neon_data_to_ecocomDP.BEETLE.R
+++ R/map_neon_data_to_ecocomDP.BEETLE.R
@@ -152,7 +152,7 @@ map_neon_data_to_ecocomDP.BEETLE <- function(
     #create observation_id column
     dplyr::group_by(sampleID) %>%
     dplyr::mutate(observation_id = paste(sampleID, row_number(), sep = ".")) %>%
-    dplyr::mutate(package_id = NA) %>%
+    dplyr::mutate(package_id = paste0(neon.data.product.id, ".", format(Sys.time(), "%Y%m%d%H%M%S"))) %>%
     dplyr:: select(observation_id, 
                    event_id = sampleID, 
                    package_id,
