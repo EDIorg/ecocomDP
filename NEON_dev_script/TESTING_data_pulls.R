87406c9e9c067fff2ae2267fb108588f34572f06
Eric R. Sokol <sokole@gmail.com>
2020-09-04 18:08:26 -0600

    add flatten_ecocomDP where given a list of ecocomDP tables, will merge and flatten into one table

--- NEON_dev_script/TESTING_data_pulls.R
+++ NEON_dev_script/TESTING_data_pulls.R
@@ -17,3 +17,19 @@ my_data_package <- ecocomDP:::read_data_edi("edi.290.1")
 
 my_data_package <- ecocomDP::read_data("DP1.20120.001",
                                        token = Sys.getenv("NEON_TOKEN"))
+
+# read in data using ecocomDP
+all_data <- ecocomDP::read_data(
+  id = "DP1.20120.001",
+  site= c('COMO','LECO'),
+  startdate = "2019-06",
+  enddate = "2019-09",
+  token = Sys.getenv("NEON_TOKEN"))
+
+my_list_of_tables <- all_data[[1]]$tables
+flat_data <- ecocomDP::flatten_ecocomDP(my_list_of_tables)
+
+
+all_data <- ecocomDP::read_data("edi.290.1")
+my_list_of_tables <- all_data[[1]]$tables
+flat_data <- ecocomDP::flatten_ecocomDP(my_list_of_tables)
