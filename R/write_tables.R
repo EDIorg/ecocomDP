879a28e093a7a63660f8609e50952ed5f4322a3d
Colin Smith <colin.smith@wisc.edu>
2020-09-23 15:40:24 -0700

    Update write_tables()
    Use data.table::fwrite()

--- R/write_tables.R
+++ R/write_tables.R
@@ -30,82 +30,22 @@
 #'     
 #' @export
 #'
-write_tables <- function(path, sep, study.name, observation, location, 
-                         taxon, dataset_summary, observation_ancillary = NULL, 
-                         location_ancillary = NULL, taxon_ancillary = NULL, 
-                         variable_mapping = NULL){
+write_tables <- function(
+  path, sep, study.name = NULL, observation = NULL, location = NULL, 
+  taxon = NULL, dataset_summary = NULL, observation_ancillary = NULL,
+  location_ancillary = NULL, taxon_ancillary = NULL, variable_mapping = NULL) {
   
   # Validate arguments --------------------------------------------------------
   
   if (missing(path)){
-    stop('Input argument "path" is missing.')
+    stop('Input argument "path" is required.', call. = FALSE)
   }
   
   if (missing(sep)){
-    stop('Input argument "sep" is missing.')
+    stop('Input argument "sep" is required.', call. = FALSE)
   } else if (!missing(sep)){
-    if ((!stringr::str_detect(sep, ',')) & (!stringr::str_detect(sep, '\\t'))){
-      stop('Input argument "sep" must be "," or "\\t".')
-    }
-  }
-  
-  if (missing(study.name)){
-    stop('Input argument "study.name" is missing.')
-  }
-  
-  if (!missing(observation)){
-    if (class(observation) != 'data.frame'){
-      stop('Input argument "observation" must be a data.frame')
-    }
-  } else if (missing(observation)){
-    stop('Input argument "observation" is missing')
-  }
-  
-  if (!missing(location)){
-    if (class(location) != 'data.frame'){
-      stop('Input argument "location" must be a data.frame')
-    }
-  } else if (missing(location)){
-    stop('Input argument "location" is missing')
-  }
-  
-  if (!missing(taxon)){
-    if (class(taxon) != 'data.frame'){
-      stop('Input argument "taxon" must be a data.frame')
-    }
-  } else if (missing(taxon)){
-    stop('Input argument "taxon" is missing')
-  }
-  
-  if (!missing(dataset_summary)){
-    if (class(dataset_summary) != 'data.frame'){
-      stop('Input argument "dataset_summary" must be a data.frame')
-    }
-  } else if (missing(dataset_summary)){
-    stop('Input argument "dataset_summary" is missing')
-  }
-  
-  if (!is.null(observation_ancillary)){
-    if (class(observation_ancillary) != 'data.frame'){
-      stop('Input argument "observation_ancillary" must be a data.frame')
-    }
-  }
-  
-  if (!is.null(location_ancillary)){
-    if (class(location_ancillary) != 'data.frame'){
-      stop('Input argument "location_ancillary" must be a data.frame')
-    }
-  }
-  
-  if (!is.null(taxon_ancillary)){
-    if (class(taxon_ancillary) != 'data.frame'){
-      stop('Input argument "taxon_ancillary" must be a data.frame')
-    }
-  }
-  
-  if (!is.null(variable_mapping)){
-    if (class(variable_mapping) != 'data.frame'){
-      stop('Input argument "variable_mapping" must be a data.frame')
+    if ((!stringr::str_detect(sep, ',')) & (!stringr::str_detect(sep, '\\t'))) {
+      stop('Input argument "sep" must be "," or "\\t".', call. = FALSE)
     }
   }
   
@@ -113,300 +53,98 @@ write_tables <- function(path, sep, study.name, observation, location,
   
   message('Writing tables to file:')
   
-  # observation
-  
-  if (sep == ','){
-    
-    message(paste0(study.name, '_observation.csv'))
-    
-    write.csv(
-      observation,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_observation.csv'),
-      row.names = F
-    )
-    
-  } else if (sep == '\t'){
-    
-    message(paste0(study.name, '_observation.txt'))
-    
-    write.table(
-      observation,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_observation.txt'
-      ),
-      col.names = T,
-      row.names = F,
-      sep = "\t"
-    )
-    
+  if (sep == ",") {
+    suffix <- ".csv"
+  } else if (sep == "\\t") {
+    suffix <- ".txt"
   }
   
-  # location
-  
-  if (sep == ','){
-    
-    message(paste0(study.name, '_location.csv'))
-    
-    write.csv(
-      location,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_location.csv'),
-      row.names = F
-    )
-    
-  } else if (sep == '\t'){
-    
-    message(paste0(study.name, '_location.txt'))
-    
-    write.table(
-      location,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_location.txt'
-      ),
-      col.names = T,
-      row.names = F,
-      sep = "\t"
-    )
-    
+  if (!is.null(observation)) {
+    message("observation")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("observation", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = observation, file = f, sep = sep, na = "NA")
   }
   
-  # taxon
-  
-  if (sep == ','){
-    
-    message(paste0(study.name, '_taxon.csv'))
-    
-    write.csv(
-      taxon,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_taxon.csv'),
-      row.names = F
-    )
-    
-  } else if (sep == '\t'){
-    
-    message(paste0(study.name, '_taxon.txt'))
-    
-    write.table(
-      taxon,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_taxon.txt'
-      ),
-      col.names = T,
-      row.names = F,
-      sep = "\t"
-    )
-    
+  if (!is.null(location)) {
+    message("location")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("location", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = location, file = f, sep = sep, na = "NA")
   }
   
-  # dataset_summary
-  
-  if (sep == ','){
-    
-    message(paste0(study.name, '_dataset_summary.csv'))
-    
-    write.csv(
-      dataset_summary,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_dataset_summary.csv'),
-      row.names = F
-    )
-    
-  } else if (sep == '\t'){
-    
-    message(paste0(study.name, '_dataset_summary.txt'))
-    
-    write.table(
-      dataset_summary,
-      file = paste0(
-        path,
-        '/',
-        study.name,
-        '_dataset_summary.txt'
-      ),
-      col.names = T,
-      row.names = F,
-      sep = "\t"
-    )
-    
+  if (!is.null(taxon)) {
+    message("taxon")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("taxon", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = taxon, file = f, sep = sep, na = "NA")
   }
   
-  # observation_ancillary
-  
-  if (!is.null(observation_ancillary)){
-    
-    if (sep == ','){
-      
-      message(paste0(study.name, '_observation_ancillary.csv'))
-      
-      write.csv(
-        observation_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_observation_ancillary.csv'),
-        row.names = F
-      )
-      
-    } else if (sep == '\t'){
-      
-      message(paste0(study.name, '_observation_ancillary.txt'))
-      
-      write.table(
-        observation_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_observation_ancillary.txt'
-        ),
-        col.names = T,
-        row.names = F,
-        sep = "\t"
-      )
-      
-    }
-    
+  if (!is.null(dataset_summary)) {
+    message("dataset_summary")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("dataset_summary", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = dataset_summary, file = f, sep = sep, na = "NA")
   }
   
-  # location_ancillary
-  
-  if (!is.null(location_ancillary)){
-    
-    if (sep == ','){
-      
-      message(paste0(study.name, '_location_ancillary.csv'))
-      
-      write.csv(
-        location_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_location_ancillary.csv'),
-        row.names = F
-      )
-      
-    } else if (sep == '\t'){
-      
-      message(paste0(study.name, '_location_ancillary.txt'))
-      
-      write.table(
-        location_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_location_ancillary.txt'
-        ),
-        col.names = T,
-        row.names = F,
-        sep = "\t"
-      )
-      
-    } 
-    
+  if (!is.null(observation_ancillary)) {
+    message("observation_ancillary")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("observation_ancillary", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = observation_ancillary, file = f, sep = sep, na = "NA")
   }
   
-  # taxon_ancillary
-  
-  if (!is.null(taxon_ancillary)){
-    
-    if (sep == ','){
-      
-      message(paste0(study.name, '_taxon_ancillary.csv'))
-      
-      write.csv(
-        taxon_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_taxon_ancillary.csv'),
-        row.names = F
-      )
-      
-    } else if (sep == '\t'){
-      
-      message(paste0(study.name, '_taxon_ancillary.txt'))
-      
-      write.table(
-        taxon_ancillary,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_taxon_ancillary.txt'
-        ),
-        col.names = T,
-        row.names = F,
-        sep = "\t"
-      )
-      
-    }
-    
+  if (!is.null(location_ancillary)) {
+    message("location_ancillary")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("location_ancillary", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = location_ancillary, file = f, sep = sep, na = "NA")
   }
   
-  # variable_mapping
+  if (!is.null(taxon_ancillary)) {
+    message("taxon_ancillary")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("taxon_ancillary", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = taxon_ancillary, file = f, sep = sep, na = "NA")
+  }
   
-  if (!is.null(variable_mapping)){
-    
-    if (sep == ','){
-      
-      message(paste0(study.name, '_variable_mapping.csv'))
-      
-      write.csv(
-        variable_mapping,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_variable_mapping.csv'),
-        row.names = F
-      )
-      
-    } else if (sep == '\t'){
-      
-      message(paste0(study.name, '_variable_mapping.txt'))
-      
-      write.table(
-        variable_mapping,
-        file = paste0(
-          path,
-          '/',
-          study.name,
-          '_variable_mapping.txt'
-        ),
-        col.names = T,
-        row.names = F,
-        sep = "\t"
-      )
-      
-    }
-    
+  if (!is.null(variable_mapping)) {
+    message("variable_mapping")
+    f <- paste0(
+      path, 
+      "/", 
+      paste0(
+        c(study.name, paste0("variable_mapping", suffix)), 
+        collapse = "_"))
+    data.table::fwrite(x = variable_mapping, file = f, sep = sep, na = "NA")
   }
 
 }
