55f7ae8dc0f244c27316cf140d76516cc479da55
Margaret O'Brien <margaret.obrien@ucsb.edu>
2020-12-15 19:30:32 -0800

    coverage data from EDI and NEON included. stacked bar plots

--- paper_2020/plot_coverage_2020.R
+++ paper_2020/plot_coverage_2020.R
@@ -13,24 +13,42 @@ setwd('/Users/mob/Desktop/EDI_files_at_msi/EDI/thematic_standardization/ecocomDP
 library(ggplot2)
 library(cowplot)
 library(readr)
+library(dplyr)
 
 
 # load data:
 NEON_dataset_coverage <- read_csv("NEON_dataset_coverage.csv")
+EDI_dataset_coverage <- read_csv("EDI_dataset_coverage.csv") 
+## edit this file manually to remove some extra fields in rows 42, 60, 61
+
+#, col_types = ("ccnnnnnnnnnc"))
 
 
 # manipulate data
 # calc temporal cov for NEON
-NEON_dataset_coverage$num_years <- (NEON_dataset_coverage$max_year - NEON_dataset_coverage$min_year) +1
+# NEON_dataset_coverage$num_years <- (NEON_dataset_coverage$max_year - NEON_dataset_coverage$min_year) +1
 
 # combine eric, colin files
-# TO DO, Tuesday
-combined_data <- NEON_dataset_coverage
+# grab, rename a subset of incoming files for temporal, geo coverage.
+NEON_1 <- data.frame(NEON_dataset_coverage$dpid, NEON_dataset_coverage$site_id, NEON_dataset_coverage$n_years, NEON_dataset_coverage$std_dev_interval_betw_years, NEON_dataset_coverage$areaKm2 )
+colnames(NEON_1) <- c("L1_id", "site_id", "n_years", "sd_year_interval", "area_km2" )
+NEON_1$area_m2 = NA
 
+EDI_1 <- data.frame(EDI_dataset_coverage$L1_id, EDI_dataset_coverage$L0_id, EDI_dataset_coverage$n_years, EDI_dataset_coverage$std_dev_interval_betw_years, EDI_dataset_coverage$geo_extent_bounding_box_m2)
+colnames(EDI_1) <- c("L1_id", "site_id", "n_years", "sd_year_interval", "area_m2" )
+EDI_1$area_km2 <- EDI_dataset_coverage$geo_extent_bounding_box_m2/1000000
+
+combined_geo_temporal_cov <- dplyr::bind_rows(NEON_1, EDI_1)
 
 # add colum for source
-combined_data$source <- ifelse(grepl("DP1.", combined_data$dpid, ignore.case = F), "NEON", 
-                ifelse(grepl("edi.", combined_data$dpid, ignore.case = F), "EDI", "Other"))
+combined_geo_temporal_cov$Source <- ifelse(grepl("DP1.", combined_geo_temporal_cov$L1_id, ignore.case = F), "NEON", 
+                ifelse(grepl("edi.", combined_geo_temporal_cov$L1_id, ignore.case = F), "EDI", 
+                       ifelse(grepl("knb-lter", combined_geo_temporal_cov$L1_id, ignore.case = F), "EDI", 
+                               "Other")))
+
+
+
+
 
 # add column for taxonomic groups
 combined_data$taxon_label <- ifelse(grepl("algae", combined_data$taxon_type, ignore.case = T), "Algae", 
@@ -50,16 +68,30 @@ combined_data$taxon_label <- ifelse(grepl("algae", combined_data$taxon_type, ign
 # plots
 
 # geographic coverage
-plot_geoCov <- ggplot(combined_data, aes(x=areaKm2)) + 
-  geom_histogram( binwidth=15, fill="#69b3a2", color="#e9ecef", alpha=0.7) +
+plot_geoCov <- ggplot(combined_geo_temporal_cov, aes(x=log10(area_km2), fill=Source ) ) + 
+ # geom_histogram( binwidth=1, fill="#69b3a2", color="#e9ecef", alpha=0.7) +
+  geom_histogram( binwidth=.5) +
   # ggtitle("Bin size = 15") +
   theme_minimal_hgrid()  +
   ylab("Number of Datasets") + 
-  xlab("Area, km^2")
+  xlab("Log 10 (Area, km^2)")
 
-# temporal coverage
-plot_temCov <-   ggplot(combined_data, aes(x=num_years)) + 
-  geom_histogram( binwidth=1, fill="red", color="red", alpha=0.6) +
+
+# geo plot with 2 axes:
+# might work: https://www.r-graph-gallery.com/line-chart-dual-Y-axis-ggplot2.html
+
+
+
+
+
+
+
+
+
+# temporal coverage IN PROGRESS
+plot_temCov <-   ggplot(combined_geo_temporal_cov, aes(x=n_years, fill=Source)) + 
+#   geom_histogram( binwidth=1, fill="red", color="red", alpha=0.6) +
+  geom_histogram( binwidth=3) +
   theme_minimal_hgrid()  +
   ylab("Number of Datasets") + 
   xlab("Duration, years")      
@@ -67,7 +99,12 @@ plot_temCov <-   ggplot(combined_data, aes(x=num_years)) +
                 
 # temporal evenness
 # place holder.
-plot_temEvenness <- plot_temCov
+plot_temEvenness <- ggplot(combined_geo_temporal_cov, aes(x=sd_year_interval, fill=Source)) + 
+#   geom_histogram( binwidth=.5, fill="green", color="green", alpha=0.6) +
+  geom_histogram( binwidth=.3) +
+  theme_minimal_hgrid()  +
+  ylab("") + 
+  xlab("S.D. interval between years")
 
 # taxonomic coverage
 # with hints from:
@@ -85,4 +122,6 @@ plot_taxonCov <- ggplot(data=combined_data, aes(x=taxon_label)) +
 # arrange plots
 # cowplot recommendation from: https://stackoverflow.com/questions/1249548/side-by-side-plots-with-ggplot2
 p <- plot_grid(plot_temCov, plot_temEvenness, plot_geoCov, plot_taxonCov, labels = "AUTO")
+p <- plot_grid(plot_temCov, plot_temEvenness, plot_geoCov, labels = "AUTO")
+
 ggsave("fig_x.png", p)
