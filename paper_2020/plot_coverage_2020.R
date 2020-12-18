235c0b441724c2bc4836b129f17c2b08cdb3fb1d
Margaret O'Brien <margaret.obrien@ucsb.edu>
2020-12-18 12:33:57 -0800

    added asterisk to tick, mosquito labels. repaired grepping of tick that classifed tick_pathogen with ticks instead of Other

--- paper_2020/plot_coverage_2020.R
+++ paper_2020/plot_coverage_2020.R
@@ -43,12 +43,14 @@ EDI_1$area_km2 <- EDI_dataset_coverage$geo_extent_bounding_box_m2/1000000
 
 combined_geo_temporal_cov <- dplyr::bind_rows(NEON_1, EDI_1)
 
-# add colum for source
+# add column for source
 combined_geo_temporal_cov$Source <- ifelse(grepl("DP1.", combined_geo_temporal_cov$L1_id, ignore.case = F), "NEON", 
                 ifelse(grepl("edi.", combined_geo_temporal_cov$L1_id, ignore.case = F), "EDI", 
                        ifelse(grepl("knb-lter", combined_geo_temporal_cov$L1_id, ignore.case = F), "EDI", 
                                "Other")))
 
+# 
+# taxon data --------------------------------------------
 # Get EDI taxonomic info into long form
 df <- dplyr::select(EDI_dataset_coverage, L1_id, class)
 
@@ -67,39 +69,123 @@ for (i in 1:nrow(df)) {
       class = classes,
       stringsAsFactors = FALSE))
 }
+# remove the first NA line (during setup); remove the entries with NA
 res <- res[-1, ]
 res <- res[res$class != "NA", ]
+# add a column for labels
+res$taxon_label <- NA
 
-# Write to a file and manually add missing taxa classes
-# Add code here .....
+# Write to a file and manually add missing taxa classes or add code here .....
+res$taxon_label <- 
+  ifelse(grepl("Actinopterygii", res$class, ignore.case = T), "Fish", 
+         ifelse(grepl("Amphibia", res$class, ignore.case = T), "Amphibian",
+                ifelse(grepl("Anthozoa", res$class, ignore.case = T), "Cnidarian",  # Coral/Anemone
+                ifelse(grepl("Arachnida", res$class, ignore.case = T), "Arthropod",
+                ifelse(grepl("Aves", res$class, ignore.case = T), "Bird",
+                ifelse(grepl("Bacillariophyceae|Chlorophyceae|Cyanophyceae|Florideophyceae|Phaeophyceae", res$class, ignore.case = T), "Algae",
+                ifelse(grepl("Bivalvia|Cephalopoda", res$class, ignore.case = T), "Mollusc",
+                ifelse(grepl("Branchiopoda", res$class, ignore.case = T), "Arthropod", # crustacean
+                ifelse(grepl("Chilopoda", res$class, ignore.case = T), "Other terrestrial invertebrate",
+                ifelse(grepl("Bryopsida", res$class, ignore.case = T), "Plant",
+                ifelse(grepl("Calcarea", res$class, ignore.case = T), "Sponge",
+                ifelse(grepl("Chondrichthyes|Chondrostei|Elasmobranchii", res$class, ignore.case = T), "Fish",
+                ifelse(grepl("Chondrostei", res$class, ignore.case = T), "Fish",
+                ifelse(grepl("Clitellata", res$class, ignore.case = T), "Worm", # terrestrial or aquatic?
+                ifelse(grepl("Collembola", res$class, ignore.case = T), "Arthropod", # centipede
+                ifelse(grepl("Cycadopsida", res$class, ignore.case = T), "Plant",
+                ifelse(grepl("Demospongiae", res$class, ignore.case = T), "Sponge",
+                ifelse(grepl("Diplopoda", res$class, ignore.case = T), "Arthropod",
+                ifelse(grepl("Echinoidea", res$class, ignore.case = T), "Echinoderm",
+                ifelse(grepl("Ascidiacea|Entoprocta", res$class, ignore.case = T), "Other aquatic invertebrate",
+                ifelse(grepl("Euchelicerata", res$class, ignore.case = T), "Arthropod",
+                ifelse(grepl("Gastropoda", res$class, ignore.case = T), "Mollusc",
+                ifelse(grepl("Granuloreticulosea", res$class, ignore.case = T), "Other", #Protozoan
+                ifelse(grepl("Gymnolaemata", res$class, ignore.case = T), "Other aquatic invertebrate",
+                ifelse(grepl("Holothuroidea", res$class, ignore.case = T), "Echinoderm",                                                          
+                ifelse(grepl("Holostei", res$class, ignore.case = T), "Fish",                                                          
+                ifelse(grepl("Hydrozoa", res$class, ignore.case = T), "Cnidarian",                                                          
+                ifelse(grepl("Insecta", res$class, ignore.case = T), "Arthropod",                                                          
+                ifelse(grepl("Jungermanniopsida", res$class, ignore.case = T), "Plant", # liverwort                                                          
+                ifelse(grepl("Lycopodiopsida", res$class, ignore.case = T), "Plant", # herbaceous                                                          
+                ifelse(grepl("Magnoliopsida|Gnetopsida", res$class, ignore.case = T), "Plant",                                                          
+                ifelse(grepl("Malacostraca", res$class, ignore.case = T), "Arthropod", # crustacean                                                          
+                ifelse(grepl("Mammalia", res$class, ignore.case = T), "Mammal",                                                           
+                ifelse(grepl("Maxillopoda", res$class, ignore.case = T), "Arthropod", # crustaceans (copepods, barnacles)                                                          
+                ifelse(grepl("Ophiuroidea|Asteroidea", res$class, ignore.case = T), "Echinoderm",                                                          
+                ifelse(grepl("Ostracoda", res$class, ignore.case = T), "Arthropod", # crustacean                                                          
+                ifelse(grepl("Pinopsida", res$class, ignore.case = T), "Plant", # cone bearing                                                          
+                ifelse(grepl("Polychaeta", res$class, ignore.case = T), "Worm",                                                          
+                ifelse(grepl("Polypodiopsida", res$class, ignore.case = T), "Plant", # fern                                                          
+                ifelse(grepl("Polytrichopsida", res$class, ignore.case = T), "Plant", # mosses                                                          
+                ifelse(grepl("Reptilia", res$class, ignore.case = T), "Reptile",                                                          
+                ifelse(grepl("Sagittoidea", res$class, ignore.case = T), "Worm", # not annelid                                                          
+                ifelse(grepl("Sphagnopsida", res$class, ignore.case = T), "Plant", # mosses                                                          
+                ifelse(grepl("Stenolaemata", res$class, ignore.case = T), "Other aquatic invertebrate", # bryozoan                                                          
+                ifelse(grepl("Teleostei", res$class, ignore.case = T), "Fish",                                                           
+                ifelse(grepl("Trepaxonemata", res$class, ignore.case = T), "Other",                # Flatworm                                           
+                ifelse(grepl("Ulvophyceae", res$class, ignore.case = T), "Algae",                                                           
+                          NA
+                )))))))))))))))))))))))))))))))))))))))))))))))
+         
+# set Labels for the datasets with no class avialable:
+res <- within(res, taxon_label[L1_id == "edi.351.1"] <- "Plant")
+res <- within(res, taxon_label[L1_id == "edi.328.1"] <- "Arthropod")
+res <- within(res, taxon_label[L1_id == "edi.350.1"] <- "Other aquatic invertebrate")
+res <- within(res, taxon_label[L1_id == "edi.338.1"] <- "Algae") #phytoplankton
+res <- within(res, taxon_label[L1_id == "edi.349.1"] <- "Arthropod") #Lepidoptera
+res <- within(res, taxon_label[L1_id == "edi.342.1"] <- "Plant")
+res <- within(res, taxon_label[L1_id == "edi.333.1"] <- "Arthropod")
+res <- within(res, taxon_label[L1_id == "knb-lter-ntl.344.5"] <- "Other")  # Microbe (OTU)
+
+EDI_2_taxon <- res
+                                     
+#      ifelse(grepl("Cyanophyceae", res$class, ignore.case = T), "Algae",
+#   ifelse(grepl("Ascidiacea", res$class, ignore.case = T), "Tunicate",
+#   ifelse(grepl("Phaeophyceae", res$class, ignore.case = T), "Algae",                                                           
+#     ifelse(grepl("Florideophyceae", res$class, ignore.case = T), "Algae",
+#         ifelse(grepl("Chlorophyceae", res$class, ignore.case = T), "Algae",
+#    ifelse(grepl("Elasmobranchii", res$class, ignore.case = T), "Fish",
+# ifelse(grepl("Chondrichthyes|Chondrostei", res$class, ignore.case = T), "Fish",  NA )
+
+                              
+                              
+                          
+## NEON data
+NEON_2_taxon <- data.frame(NEON_dataset_coverage$dpid, NEON_dataset_coverage$taxon_type)
+colnames(NEON_2_taxon) <- c("L1_id", "class" )
 
 
-# taxon data:
-# df %>% separate(x, c("A", "B"))
-# not working:  foo <- data.frame(EDI_dataset_coverage$L1_id, EDI_dataset_coverage$class, stringsAsFactors = FALSE)
+
+# add column for taxonomic groups -- NEON
+NEON_2_taxon$taxon_label <- 
+  ifelse(grepl("algae", NEON_2_taxon$class, ignore.case = T), "Algae", 
+         ifelse(grepl("macroinvertebrate", NEON_2_taxon$class, ignore.case = T), "Other terrestrial invertebrate", 
+                ifelse(grepl("beetle", NEON_2_taxon$class, ignore.case = T), "Arthropod", 
+                       ifelse(grepl("bird", NEON_2_taxon$class, ignore.case = T), "Bird", 
+                              ifelse(grepl("fish", NEON_2_taxon$class, ignore.case = T), "Fish", 
+                                     ifelse(grepl("mosquito", NEON_2_taxon$class, ignore.case = T), "Mosquito*", 
+                                            ifelse(grepl("plant", NEON_2_taxon$class, ignore.case = T), "Plant", 
+                                                   ifelse(grepl("small_mammal", NEON_2_taxon$class, ignore.case = T), "Mammal", 
+                                                        ifelse(grepl("tick_pathogen", NEON_2_taxon$class, ignore.case = T), "Other", 
+                                                              ifelse(grepl("tick", NEON_2_taxon$class, ignore.case = F), "Tick*", 
+                                                                    "Other"))))))))))
 
 
-
-# add column for taxonomic groups
-combined_data$taxon_label <- ifelse(grepl("algae", combined_data$taxon_type, ignore.case = T), "Algae", 
-                      ifelse(grepl("macroinvertebrate", combined_data$taxon_type, ignore.case = T), "Macroinvertebrate", 
-                             ifelse(grepl("beetle", combined_data$taxon_type, ignore.case = T), "Beetle", 
-                                    ifelse(grepl("bird", combined_data$taxon_type, ignore.case = T), "Bird", 
-                                           ifelse(grepl("fish", combined_data$taxon_type, ignore.case = T), "Fish", 
-                                                  ifelse(grepl("mosquito", combined_data$taxon_type, ignore.case = T), "Mosquito", 
-                                                         ifelse(grepl("plant", combined_data$taxon_type, ignore.case = T), "Plant", 
-                                                                ifelse(grepl("small_mammal", combined_data$taxon_type, ignore.case = T), "Small Mammal", 
-                                                                       ifelse(grepl("Tick", combined_data$taxon_type, ignore.case = T), "Tick", 
-                                                                              ifelse(grepl("tick_pathogen", combined_data$taxon_type, ignore.case = T), "Tick Pathogen", 
-                             "Other"))))))))))
+## Combine edi, neon taxon coverage
+combined_taxon_cov <- dplyr::bind_rows(NEON_2_taxon, EDI_2_taxon)
+# add column for source
+combined_taxon_cov$Source <- 
+  ifelse(grepl("DP1.", combined_taxon_cov$L1_id, ignore.case = F), "NEON", 
+         ifelse(grepl("edi.", combined_taxon_cov$L1_id, ignore.case = F), "EDI", 
+                ifelse(grepl("knb-lter", combined_taxon_cov$L1_id, ignore.case = F), "EDI", 
+                       "Other")))
 
 
+# plots --------------------------------
 
-# plots
-
-# geo plot with 2 axes:
+# geom plot with 2 axes:
 # might work: https://www.r-graph-gallery.com/line-chart-dual-Y-axis-ggplot2.html
-
+# also try split_axis, or something like that (so you can enlarge the lower portion)
 
 # geographic coverage
 plot_geoCov <- ggplot(combined_geo_temporal_cov, aes(x=log10(area_km2), fill=Source ) ) + 
@@ -107,16 +193,18 @@ plot_geoCov <- ggplot(combined_geo_temporal_cov, aes(x=log10(area_km2), fill=Sou
   geom_histogram( binwidth=.5) +
   # ggtitle("Bin size = 15") +
   theme_minimal_hgrid()  +
-  ylab("Number of Datasets") + 
+  theme(text = element_text(size=12) )+ 
+  ylab("") + 
   xlab("Log 10 (Area, km^2)") + 
   guides(fill=FALSE)
 
 
-# temporal coverage IN PROGRESS
+# temporal coverage 
 plot_temCov <-   ggplot(combined_geo_temporal_cov, aes(x=n_years, fill=Source)) + 
 #   geom_histogram( binwidth=1, fill="red", color="red", alpha=0.6) +
   geom_histogram( binwidth=3) +
   theme_minimal_hgrid()  +
+  theme(text = element_text(size=12) )+ 
   ylab("Number of Datasets") + 
   xlab("Duration, years")  + 
   guides(fill=FALSE) # removes legend
@@ -128,26 +216,36 @@ plot_temEvenness <- ggplot(combined_geo_temporal_cov, aes(x=sd_year_interval, fi
 #   geom_histogram( binwidth=.5, fill="green", color="green", alpha=0.6) +
   geom_histogram( binwidth=.3) +
   theme_minimal_hgrid()  +
+  theme(text = element_text(size=12) )+ 
   ylab("") + 
-  xlab("S.D. interval between years") + 
+  xlab("S.D. sampling interval") + 
   guides(fill=FALSE)
+  
 
 # taxonomic coverage
 # with hints from:
 # https://stackoverflow.com/questions/7263849/what-do-hjust-and-vjust-do-when-making-a-plot-using-ggplot/7267364
-plot_taxonCov <- ggplot(data=combined_data, aes(x=taxon_label)) +
-  geom_bar(stat="count", fill="blue", color="blue", alpha=0.5) +
+plot_taxonCov <- ggplot(data=combined_taxon_cov, aes(x=taxon_label, fill = Source)) +
+  # geom_bar(stat="count", fill="blue", color="blue", alpha=0.5) +
+  geom_bar(stat="count") +
   # scale_y_continuous(expand=c(0,0)) + # removes space between the bar and the tick mark
   theme_minimal_hgrid() + 
-  ylab("") + 
+  theme(text = element_text(size=12) )+ 
+  ylab("Number of Datasets") + 
   xlab("Taxon Group")  +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1) )  # hjust sets postition of text
-    
+  theme(axis.text.x = element_text(angle = 45, hjust = 1) ) +  # hjust sets postition of text
+  guides(fill=FALSE)
+
 
 
 # arrange plots
 # cowplot recommendation from: https://stackoverflow.com/questions/1249548/side-by-side-plots-with-ggplot2
-p <- plot_grid(plot_temCov, plot_temEvenness, plot_geoCov, plot_taxonCov, labels = "AUTO")
-p <- plot_grid(plot_temCov, plot_temEvenness, plot_geoCov, labels = "AUTO")
+# nested grid of plots. taxon on the bottom, wide, geo/temporal above, 1/3 width each.
+bottom_row <- plot_grid(plot_taxonCov, labels = c("D"), label_size = 12)
+top_row <- plot_grid(
+  plot_temCov, plot_temEvenness, plot_geoCov,  
+  labels = c("A","B","C"), label_size = 12,
+  nrow = 1)
 
+p <- plot_grid(top_row, bottom_row, nrow = 2)
 ggsave("fig_x.png", p)
