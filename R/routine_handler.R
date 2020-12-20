40c4bb9ab267adcdb61990fcdbd6bd139ee10376
Colin Smith <colin.smith@wisc.edu>
2020-12-19 18:08:09 -0800

    Dev routine_handler()

--- R/routine_handler.R
+++ R/routine_handler.R
@@ -8,29 +8,37 @@ routine_handler <- function(package.id, environment, index) {
   
   # Parameterize --------------------------------------------------------------
   
-  # Read config.R for user name and password
+  # Read config.R for user name and password (inter alia)
   source("C:\\Users\\Colin\\Documents\\EDI\\data_sets\\event_notification_workflow\\config.R")
   
-  # Get next item in SQLite ---------------------------------------------------
+  # Pull from queue -----------------------------------------------------------
   
-  r <- httr::GET("https://regan.edirepository.org/ecocom-listener/package.lternet.edu")
-  r <- httr::GET("https://regan.edirepository.org/ecocom-listener/package-s.lternet.edu")
-  r <- httr::GET("https://regan.edirepository.org/ecocom-listener/package-d.lternet.edu")
+  # Is anybody waiting?
+  prd <- httr::GET("https://regan.edirepository.org/ecocom-listener/package.lternet.edu")
+  stg <- httr::GET("https://regan.edirepository.org/ecocom-listener/package-s.lternet.edu")
+  dev <- httr::GET("https://regan.edirepository.org/ecocom-listener/package-d.lternet.edu")
   
-  # Parse response
-  # Is it L0?
-  # Is it L1? If so does it have an L2-DwC-A derivative?
+  # TODO: First in line gets processed
+  # - Set config.environment (global variable)
   
-  # Create lock file ----------------------------------------------------------
+  # TODO: Identify level
+  # - Is it L0? (A data package with a derivative possessing the "ecocomDP" keyword is an L0.)
+  # - Is it L1? If so does it have an L2-DwC-A derivative?
   
-  # Call appropriate workflow -------------------------------------------------
+  # TODO: Break if empty
+  
+  # Lock ----------------------------------------------------------------------
+  
+  # TODO: Create lock file in dir of ecocom-listener
+  
+  # Call workflow -------------------------------------------------------------
   
   if (r$id == "L0") {
     
     # update_L1(
     #   package.id.L0 = package.id, # newest L0 id arriving via event notification
     #   path = config.path, 
-    #   url = config.www, 
+    #   url = config.www,
     #   user.id = config.user.id,
     #   user.pass = config.user.pass)
     
@@ -56,16 +64,9 @@ routine_handler <- function(package.id, environment, index) {
     
   }
   
-  # Upload to repository ------------------------------------------------------
+  # Publish -------------------------------------------------------------------
   
-  # Remove from queue ---------------------------------------------------------
-  
-  r <- httr::DELETE(
-    paste0("https://regan.edirepository.org/ecocom-listener/", event_id_index))
-  
-  # Remove lock file ----------------------------------------------------------
-  
-  # TODO: Write to log --------------------------------------------------------
+  # Compile log ---------------------------------------------------------------
   
   # Message maintainers -------------------------------------------------------
   
@@ -75,7 +76,11 @@ routine_handler <- function(package.id, environment, index) {
   
   r <- clear_workspace(path = path)
   
-  # TODO: Write to log
+  # Remove from queue ---------------------------------------------------------
   
+  r <- httr::DELETE(
+    paste0("https://regan.edirepository.org/ecocom-listener/", event_id_index))
+  
+  # Unlock --------------------------------------------------------------------
   
 }
