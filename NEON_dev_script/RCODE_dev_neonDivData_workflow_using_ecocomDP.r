ba4cec5c48d12d12f881f066a2b136936e996e87
Eric Sokol (US) <esokol@battelleecology.org>
2021-03-04 18:28:34 -0800

    update script errors

--- NEON_dev_script/RCODE_dev_neonDivData_workflow_using_ecocomDP.r
+++ NEON_dev_script/RCODE_dev_neonDivData_workflow_using_ecocomDP.r
@@ -2,9 +2,10 @@
 # Eric R. Sokol 
 # 3/4/2021
 
-
+# devtools::install_github("EDIorg/EMLassemblyline")
 # devtools::install_github("EDIorg/ecocomDP")
 # library(ecocomDP)
+# devtools::document()
 library(tidyverse)
 
 
@@ -28,56 +29,58 @@ for(i in 1:nrow(neon_ecocomdp_data_list)){
   
   # initialize vars
   data_list_i <- list()
-  data_flat <- data.frame()
+  data_flat_i <- data.frame()
   
   # download data
   try({
-    data_list <- read_data(
+    data_list_i <- read_data(
       id = my_dp_id,
-      site= c('COMO','LECO'), 
-      startdate = "2017-06",
-      enddate = "2021-02",
+      # site= c('COMO','LECO'), 
+      # startdate = "2017-06",
+      # enddate = "2021-02",
       token = Sys.getenv("NEON_TOKEN"),
       check.size = FALSE)
     
-    data_flat <- data_list[[1]]$tables %>% ecocomDP::flatten_ecocomDP()
+    data_flat_i <- data_list_i[[1]]$tables %>% ecocomDP::flatten_ecocomDP()
     
   })
   
-  
-  if(length(data_list > 0) && nrow(data_flat) > 0){
+  try({
+    if(length(data_list_i) > 0 && nrow(data_flat_i) > 0){
+      
+      # get data set info for log
+      data_set_log_i <- data.frame(
+        taxon_group = neon_ecocomdp_data_list$title[i] %>% 
+          stringr::str_split(" ") %>% unlist() %>% dplyr::first(),
+        data_package_id = data_list_i[[1]]$tables$dataset_summary$package_id,
+        n_taxa = data_flat_i$taxon_id %>% unique() %>% length(),
+        sites = data_flat_i$siteID %>% unique() %>% paste(.,collapse = "|"),
+        start_date = data_flat_i$observation_datetime %>% lubridate::as_date() %>% min(),
+        end_date = data_flat_i$observation_datetime %>% lubridate::as_date() %>% max(),
+        data_package_title = neon_ecocomdp_data_list$title[i],
+        neon_ecocomdp_mapping_method = my_dp_id,
+        original_neon_data_product_id = data_list_i[[1]]$tables$dataset_summary$original_package_id)
+      
+      # write data to out dir
+      package_id <- data_list_i[[1]]$tables$dataset_summary$package_id
+      package_filename <- paste0(my_out_dir, "/",package_id,".RDS")
+      saveRDS(data_list_i, file = package_filename)
+      
+    }else{
+      data_set_log_i <- data.frame(
+        taxon_group = neon_ecocomdp_data_list$title[i] %>% 
+          stringr::str_split(" ") %>% unlist() %>% dplyr::first(),
+        data_package_id = "DATA NOT FOUND",
+        n_taxa = NA,
+        sites = NA_character_,
+        start_date = NA_character_,
+        end_date = NA_character_,
+        data_package_title = neon_ecocomdp_data_list$title[i],
+        neon_ecocomdp_mapping_method = my_dp_id,
+        original_neon_data_product_id = neon_ecocomdp_data_list$source_id[i])
+    }
     
-    # get data set info for log
-    data_set_log_i <- data.frame(
-      taxon_group = neon_ecocomdp_data_list$title[i] %>% 
-        stringr::str_split(" ") %>% unlist() %>% dplyr::first(),
-      data_package_id = data_list[[1]]$tables$dataset_summary$package_id,
-      n_taxa = data_flat$taxon_id %>% unique() %>% length(),
-      sites = data_flat$siteID %>% unique() %>% paste(.,collapse = "|"),
-      start_date = data_flat$observation_datetime %>% lubridate::as_date() %>% min(),
-      end_date = data_flat$observation_datetime %>% lubridate::as_date() %>% max(),
-      data_package_title = neon_ecocomdp_data_list$title[i],
-      neon_ecocomdp_mapping_method = my_dp_id,
-      original_neon_data_product_id = data_list[[1]]$tables$dataset_summary$original_package_id)
-    
-    # write data to out dir
-    package_id <- data_list[[1]]$tables$dataset_summary$package_id
-    package_filename <- paste0(my_out_dir, "/",package_id,".RDS")
-    saveRDS(data_list, file = package_filename)
-    
-  }else{
-    data_set_log_i <- data.frame(
-      taxon_group = neon_ecocomdp_data_list$title[i] %>% 
-        stringr::str_split(" ") %>% unlist() %>% dplyr::first(),
-      data_package_id = "DATA NOT FOUND",
-      n_taxa = NA,
-      sites = NA_character_,
-      start_date = NA_character_,
-      end_date = NA_character_,
-      data_package_title = neon_ecocomdp_data_list$title[i],
-      neon_ecocomdp_mapping_method = my_dp_id,
-      original_neon_data_product_id = neon_ecocomdp_data_list$source_id[i])
-  }
+  })
   
   
   # log data set 
@@ -94,5 +97,5 @@ my_filename <- paste0(my_out_dir, "/dataset_log.txt")
 
 readr::write_delim(
   data_set_log,
-  path = my_filename,
+  file = my_filename,
   delim = "\t")
