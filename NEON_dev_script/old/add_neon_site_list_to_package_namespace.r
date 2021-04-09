4152d3906039756d9dc593678111f675617a2803
Colin Smith <colin.smith@wisc.edu>
2021-04-09 14:17:58 -0700

    House keeping for CRAN submission

--- NEON_dev_script/old/add_neon_site_list_to_package_namespace.r
+++
@@ -1,4 +0,0 @@
-# add NEON site list to package data
-neon_site_list <- readr::read_csv("neon-field-sites.csv")
-usethis::use_data(neon_site_list,
-                  internal = TRUE)
--- paper_2020/readme.txt
+++
@@ -1,2 +0,0 @@
-Directory holds working files for the paper (e.g., data for tables and figs)
-
