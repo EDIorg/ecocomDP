75bf36cb50593d19aea492bf59a39c1a403eee4d
Colin Smith <colin.smith@wisc.edu>
2021-04-22 15:44:32 -0700

    Finish plot_data() tests

--- tests/testthat/test_plot_data.R
+++ tests/testthat/test_plot_data.R
@@ -3,22 +3,31 @@ context("plot_data()")
 library(ecocomDP)
 
 testthat::test_that("Plots are written to file", {
-  id <- "edi.193.3"
-  d <- try(suppressWarnings(read_data(id = id)), silent = TRUE)
-  if (class(d) != "try-error") {
-    # only one data object at a time
-    plot_data(d, type = "community", path = )
-    
-    save_data(d, tempdir(), file.type = ".rds", file.name = id) # .rds
-    expect_true(file.exists(fname))                             # was created
-    unlink(fname, recursive = TRUE, force = TRUE)
-    save_data(d, tempdir(), file.type = ".csv", file.name = id) # .csv
-    expect_true(dir.exists(dname))                              # was created
-    fnames <- tools::file_path_sans_ext(list.files(dname))      # dir has tables
-    expect_true(all(fnames %in% unique(criteria$table)))
-    fext <- unique(tools::file_ext(list.files(dname)))          # has file extension
-    expect_equal(fext, "csv")
-    unlink(dname, recursive = TRUE, force = TRUE)
-  }
+  d <- read_example_dataset()
+  id <- names(d)
+  dname <- paste0(tempdir(), "/plots")
+  unlink(dname, recursive = TRUE, force = TRUE)
+  dir.create(dname)
+  suppressWarnings(plot_data(d, type = "community", path = dname))
+  fnames <- list.files(dname)
+  expect_true(length(fnames) != 0)                                             # dir is not empty
+  suffix <- c("alpha_diversity", "sampling_effort", 
+              "sp_accumulation_over_space", "sp_accumulation_over_time", 
+              "sp_shared_among_sites")
+  expect_true(all(stringr::str_detect(fnames, paste(suffix, collapse = "|")))) # file suffix describes plot
+  unlink(dname, recursive = TRUE, force = TRUE)
+})
+
+
+testthat::test_that("Warns if datetimes are chars", {
+  d <- read_example_dataset(parse.datetime = FALSE)
+  id <- names(d)
+  dname <- paste0(tempdir(), "/plots")
+  unlink(dname, recursive = TRUE, force = TRUE)
+  dir.create(dname)
+  expect_warning(
+    plot_data(d, type = "community", path = dname),
+    regexp = "Input datetimes are character strings. Accuracy may be improved")
+  unlink(dname, recursive = TRUE, force = TRUE)
 })
 
