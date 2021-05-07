7b9ba835f4096a8546ef1aa3fd22ebb4543f56d6
Colin Smith <colin.smith@wisc.edu>
2021-05-06 19:19:58 -0700

    Export write_tables()

--- R/utilities.R
+++ R/utilities.R
@@ -1477,11 +1477,7 @@ vocab_resolve_terms <- function(x, cv, messages = FALSE, interactive = FALSE){
 #' @param path
 #'     (character) Directory to which the files will be written.
 #' @param sep
-#'     (character) Field delimiter used in the ecocomDP tables. Valid options 
-#'     are "," or "\\t".
-#' @param study.name
-#'     (character) Name to be prefixed to each ecocomDP table name (e.g. 
-#'     'bird_survey_').
+#'     (character) Field delimiter of returned files
 #' @param observation
 #'     (data frame) observation table
 #' @param location
@@ -1501,9 +1497,15 @@ vocab_resolve_terms <- function(x, cv, messages = FALSE, interactive = FALSE){
 #'
 #' @return 
 #'     ecocomDP tables written to file.
+#'     
+#' @export
+#' 
+#' \dontrun{
+#' 
+#' }
 #'
 write_tables <- function(
-  path, sep, study.name = NULL, observation = NULL, location = NULL, 
+  path, sep = ",", observation = NULL, location = NULL, 
   taxon = NULL, dataset_summary = NULL, observation_ancillary = NULL,
   location_ancillary = NULL, taxon_ancillary = NULL, variable_mapping = NULL) {
   
@@ -1513,109 +1515,61 @@ write_tables <- function(
     stop('Input argument "path" is required.', call. = FALSE)
   }
   
-  if (missing(sep)){
-    stop('Input argument "sep" is required.', call. = FALSE)
-  } else if (!missing(sep)){
-    if ((!stringr::str_detect(sep, ',')) & (!stringr::str_detect(sep, '\\t'))) {
-      stop('Input argument "sep" must be "," or "\\t".', call. = FALSE)
-    }
-  }
-  
   # Write tables to file
   
   message('Writing tables to file:')
   
   if (sep == ",") {
     suffix <- ".csv"
-  } else if (sep == "\\t") {
+  } else {
     suffix <- ".txt"
   }
   
   if (!is.null(observation)) {
     message("  observation")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("observation", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("observation", suffix))
     data.table::fwrite(x = observation, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(location)) {
     message("  location")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("location", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("location", suffix))
     data.table::fwrite(x = location, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(taxon)) {
     message("  taxon")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("taxon", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("taxon", suffix))
     data.table::fwrite(x = taxon, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(dataset_summary)) {
     message("  dataset_summary")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("dataset_summary", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("dataset_summary", suffix))
     data.table::fwrite(x = dataset_summary, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(observation_ancillary)) {
     message("  observation_ancillary")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("observation_ancillary", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("observation_ancillary", suffix))
     data.table::fwrite(x = observation_ancillary, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(location_ancillary)) {
     message("  location_ancillary")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("location_ancillary", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("location_ancillary", suffix))
     data.table::fwrite(x = location_ancillary, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(taxon_ancillary)) {
     message("  taxon_ancillary")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("taxon_ancillary", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("taxon_ancillary", suffix))
     data.table::fwrite(x = taxon_ancillary, file = f, sep = sep, na = "NA")
   }
   
   if (!is.null(variable_mapping)) {
     message("  variable_mapping")
-    f <- paste0(
-      path, 
-      "/", 
-      paste0(
-        c(study.name, paste0("variable_mapping", suffix)), 
-        collapse = "_"))
+    f <- paste0(path, "/", paste0("variable_mapping", suffix))
     data.table::fwrite(x = variable_mapping, file = f, sep = sep, na = "NA")
   }
   
