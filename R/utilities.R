64b442ebcdbc6ae55dcbb45d17a85b008be8f08b
Colin Smith <colin.smith@wisc.edu>
2021-05-16 07:38:00 -0700

    Consolidate funcs

--- R/utilities.R
+++ R/utilities.R
@@ -956,6 +956,26 @@ ping_edi <- function() {
 
 
 
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+
+
+
+
+
+
+
 
 
 
